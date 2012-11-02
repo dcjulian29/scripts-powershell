826e5a400c85736e106036382e06761e4e49f6e0
Julian Easterling <julian@julianscorner.com>
2012-11-02 15:32:26 -0400

    TFS development functions/aliases/settings

---
+++ GlobalScripts/20/dev-tfs.ps1
@@ -0,0 +1,22 @@
+$env:Path = "$env:Path;c:\bin\development-tools\GitTfs"
+
+$tfPath = First-Path `
+  (Find-ProgramFiles 'Microsoft Visual Studio 11.0\Common7\IDE\TF.exe'), `
+  (Find-ProgramFiles 'Microsoft Visual Studio 10.0\Common7\IDE\TF.exe')
+
+function tf()
+{
+  & $tfPath $args;
+}
+
+function tfHistory($path, $knownGoodVersion)
+{
+  if ($knownGoodVersion)
+  {
+    tf hist $path /noprompt /recursive /stopafter:$knownGoodVersion
+  }
+  else
+  {
+    tf hist $path /noprompt /recursive
+  }
+}
