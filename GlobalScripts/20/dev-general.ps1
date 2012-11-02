4a22bea43c2d8cf07fe6ffbec9c6f822b2aa3222
Julian Easterling <julian@julianscorner.com>
2012-11-02 15:27:13 -0400

    general development functions/aliases

---
+++ GlobalScripts/20/dev-general.ps1
@@ -0,0 +1,22 @@
+$env:Path = "$env:Path;c:\bin\development-tools\"
+
+function build-project
+{
+  if (test-path build.bat)
+  {
+    .\build.bat $args
+  }
+  else
+  {
+    if (test-path build.xml)
+    {
+      C:\bin\development-tools\nant\bin\NAnt.exe -buildfile:build.xml $args
+    }
+    else
+    {
+      Write-Host "This directory does not include build script to build the project"
+    }
+  }
+}
+
+Set-Alias bp build-project
