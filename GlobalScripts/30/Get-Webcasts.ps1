8cd2c6265409a3d3c81d66aa5fbc65dcb9d812d9
Julian Easterling <julian@julianscorner.com>
2013-01-09 15:32:36 -0500

    Changed the base directory and make use of the new "download to folder" option.

--- GlobalScripts/30/Get-Webcasts.ps1
+++ GlobalScripts/30/Get-Webcasts.ps1
@@ -6,10 +6,10 @@ function Get-Webcasts
   )
   
 
-  $baseDirectory = "D:\Videos"
+  $baseDirectory = "D:\Webcasts"
   $opmlFile = $baseDirectory + "\webcasts.opml"
 
   $opml = [xml](Get-Content $opmlFile)
   $opml.opml.body.outline | `
-    foreach { Get-WebcastsFromRSS $_.xmlUrl $baseDirectory $downloadFiles }
+    foreach { Get-WebcastsFromRSS $_.xmlUrl $baseDirectory $downloadFiles $true }
 }
