d37afa8a42fdc9aede085f4f89e7c3801d17f318
Julian Easterling <julian@julianscorner.com>
2013-01-09 15:27:37 -0500

    Added some aliases to go directly to some of my common folders.

---
+++ GlobalScripts/20/alias.ps1
@@ -0,0 +1,7 @@
+function go-home { Set-Location $env:home }
+function go-desktop { Set-Location $env:home\Desktop }
+function go-download { Set-Location $env:home\Downloads }
+function go-documents { Set-Location $env:home\Documents }
+function go-pictures { Set-Location $env:home\Pictures }
+function go-videos { Set-Location $env:home\Videos }
+function go-webcasts { Set-Location D:\Webcasts }
