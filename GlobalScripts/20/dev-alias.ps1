19874765a78701b2f0c00d0d85fa918161cfb1cd
Julian Easterling <julian@julianscorner.com>
2013-03-10 13:52:51 -0400

    Added some aliases for dev functions

---
+++ GlobalScripts/20/dev-alias.ps1
@@ -0,0 +1,6 @@
+function dev-aamva { Set-Location D:\aamva\dev }
+function dev-jcog { Set-Location D:\jcog\dev }
+function dev-jnet { Set-Location D:\jnet\dev }
+function dev-marriott { Set-Location D:\marriott\dev }
+
+function kvs { Stop-Process -ProcessName devenv }
