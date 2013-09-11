bd4f810076dbc905d34b5f9e35b237cc38d86d15
Julian Easterling <julian@julianscorner.com>
2013-09-10 23:32:26 -0400

    updated aliases to check for the existence of the directory before trying to change to that directory.

--- GlobalScripts/20/dev-alias.ps1
+++ GlobalScripts/20/dev-alias.ps1
@@ -1,9 +1,62 @@
-function go-dev { Set-Location C:\dev }
-function dev-aamva { Set-Location D:\aamva\dev }
-function dev-jcog { Set-Location D:\jcog\dev }
-function dev-jnet { Set-Location D:\jnet\dev }
-function dev-marriott { Set-Location D:\marriott\dev }
-function dev-wiki { Set-Location D:\jnet\wiki }
+function go-dev 
+{
+  if (Test-Path "C:\dev")
+  {
+    Set-Location C:\dev
+  }
+  else
+  {
+    Write-Error "C:\Dev does not exist... Maybe a more specific development directory is needed?"
+  }
+}
+
+function dev-fei 
+{
+  if (Test-Path "D:\fei\dev")
+  {
+    Set-Location D:\fei\dev
+  }
+  else
+  {
+    Write-Error "FEi Development Directory does not exist..."
+  }
+}
+
+function dev-jcog
+{
+  if (Test-Path "D:\jcog\dev")
+  {
+    Set-Location D:\jcog\dev
+  }
+  else
+  {
+    Write-Error "JCoG Development Directory does not exist..."
+  }
+}
+
+function dev-jnet
+{
+  if (Test-Path "D:\jnet\dev")
+  {
+    Set-Location D:\jnet\dev
+  }
+  else
+  {
+    Write-Error "JNet Development Directory does not exist..."
+  }
+}
+
+function dev-wiki
+{
+  if (Test-Path "D:\jnet\wiki")
+  {
+    Set-Location D:\jnet\wiki
+  }
+  else
+  {
+    Write-Error "JNet Wiki Directory does not exist..."
+  }
+}
 
 function kvs { Stop-Process -ProcessName devenv }
 
