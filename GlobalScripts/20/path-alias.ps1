4935124e6c7d04c81a116ac0babdb9bb9d6edda5
Julian Easterling <julian@julianscorner.com>
2013-09-10 23:34:16 -0400

    Added a couple of functions that mimic some batch files that manipulate the path. Since a CMD shell is created to run the batch file, then destroyed afterward, the path in PowerShell never got updated...

---
+++ GlobalScripts/20/path-alias.ps1
@@ -0,0 +1,38 @@
+function path-cygwin { $env:Path = "C:\cygwin\bin;$($env:Path)" }
+function path-mongo { $env:Path = "C:\Program Files\MongoDB\bin;$($env:Path)" }
+function path-nodejs { $env:Path = "$($env:PF32)\nodejs;$($env:Path)" }
+
+function path-java 
+{ 
+  $java = ""
+
+  if (Test-Path "C:\Program Files (x86)\Java\jre6")
+  {
+    $java = "C:\Program Files (x86)\Java\jre6"
+  }
+
+  if (Test-Path "C:\Program Files (x86)\Java\jre7")
+  {
+    $java = "C:\Program Files (x86)\Java\jre7"
+  }
+
+  if (Test-Path "C:\Program Files\Java\jre6")
+  {
+    $java = "C:\Program Files\Java\jre6"
+  }
+
+  if (Test-Path "C:\Program Files\Java\jre7")
+  {
+    $java = "C:\Program Files\Java\jre7"
+  }
+
+  if ($java.Length -gt 0)
+  {
+    $env:JAVA_HOME = $java
+    $env:Path = "$java\bin;$($env:Path)"    
+  }
+  else
+  {
+    Write-Error "Java Runtime Not Found!"
+  }
+}
