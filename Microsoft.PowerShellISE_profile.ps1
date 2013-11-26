770c84d27a6e538f4b2695793ea0e700a55aef4c
Julian Easterling <julian@julianscorner.com>
2013-11-26 18:33:49 -0500

    PowerShell ISE needs to load "Global Scripts" as well.

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -1,5 +1,6 @@
 # PowerShell ISE Profile Script
 
 . "$(Split-Path $profile)\Load-ProfileModulesAndScripts.ps1" PowerShellISE
+. "$(Split-Path $profile)\Load-ProfileModulesAndScripts.ps1" GlobalScripts
 
 Set-Location C:\
