e493931089c9365b9f1952502cfc158f4dd27150
Julian Easterling <julian@julianscorner.com>
2014-01-09 09:16:24 -0500

    Updated this profile script to only make changes when it detects GIT and/or POSH-GIT module is installed.

--- GlobalScripts/20/dev-git.ps1
+++ GlobalScripts/20/dev-git.ps1
@@ -1,28 +1,26 @@
-if (Test-Path "$env:SYSTEMDRIVE\Program Files (x86)\")
-{
-  $GIT_INSTALL_ROOT = "$env:SYSTEMDRIVE\Program Files (x86)\Git\bin"
-}
-else
-{
-  $GIT_INSTALL_ROOT = "$env:SYSTEMDRIVE\Program Files\Git\bin"
+if (Test-Path "$env:SYSTEMDRIVE\Program Files (x86)\") {
+    $GIT_INSTALL_ROOT = "$env:SYSTEMDRIVE\Program Files (x86)\Git\bin"
+} else {
+    $GIT_INSTALL_ROOT = "$env:SYSTEMDRIVE\Program Files\Git\bin"
 }
 
-if (Test-Path "$GIT_INSTALL_ROOT")
-{
-  $env:Path = "$env:Path;$GIT_INSTALL_ROOT"
+if (Test-Path "$GIT_INSTALL_ROOT") {
+    $env:Path = "$env:Path;$GIT_INSTALL_ROOT"
 
-  Import-Module Posh-Git
+    if (Test-Path "$($env:UserProfile)\Documents\WindowsPowerShell\Modules\posh-git") {
+        Import-Module Posh-Git
 
-  Enable-GitColors
+        Enable-GitColors
 
-  $GitPromptSettings.BeforeText = "["
+        $GitPromptSettings.BeforeText = "["
+    }
+    
+    $GIT = "$GIT_INSTALL_ROOT\git.exe"
 
-  $GIT = "$GIT_INSTALL_ROOT\git.exe"
+    function gb { ."C:\bin\development-tools\git-backup.bat" }
+    function gbr { ."C:\bin\development-tools\git-backup-remove.bat" }
 
-  function gb { ."C:\bin\development-tools\git-backup.bat" }
-  function gbr { ."C:\bin\development-tools\git-backup-remove.bat" }
-
-  function gpull { cmd.exe /c "$GIT" pull }
-  function gpush { cmd.exe /c "$GIT" push }
-  function gs { cmd.exe /c "$GIT" status }
+    function gpull { cmd.exe /c "$GIT" pull }
+    function gpush { cmd.exe /c "$GIT" push }
+    function gs { cmd.exe /c "$GIT" status }
 }
