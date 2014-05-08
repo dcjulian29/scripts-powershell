3902555cb1612d86b134c3459839465835e6c825
Julian Easterling <julian@julianscorner.com>
2014-05-07 20:29:10 -0400

    Added a "Format-XML" function to "pretty-print" XML.

---
+++ GlobalScripts/Text/TextFormatting.psm1
@@ -0,0 +1,22 @@
+﻿Function Format-XML {
+    param (
+        [xml] $xml,
+        [int] $indent = 4
+    )
+
+    $stringWriter = New-Object System.IO.StringWriter
+
+    $writer = New-Object System.Xml.XmlTextWriter $stringWriter
+    $writer.Formatting = "indented"
+    $writer.Indentation = $indent
+
+    $xml.WriteContentTo($writer)
+
+    $writer.Flush()
+    $StringWriter.Flush()
+
+    Write-Output $StringWriter.ToString()
+}
+
+
+Export-ModuleMember Format-Xml
