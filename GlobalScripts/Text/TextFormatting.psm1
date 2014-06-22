ca87502dc9125aac5c254515fdf89dad66cab744
Julian Easterling <julian@julianscorner.com>
2014-06-22 10:03:58 -0400

    updated format-xml to do new lines on attributes by default. added a Format JSON function to pretty-print json files.

--- GlobalScripts/Text/TextFormatting.psm1
+++ GlobalScripts/Text/TextFormatting.psm1
@@ -1,22 +1,75 @@
 ﻿Function Format-XML {
     param (
-        [xml] $xml,
-        [int] $indent = 4
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        [psobject] $Xml,
+        [int] $Indent = 4,
+        [switch] $NoNewLineOnAttribute
     )
+    
+    BEGIN {
+        [System.Reflection.Assembly]::LoadWithPartialName("System.Xml") | Out-Null
+        $XmlText = ""
+    }
+    
+    PROCESS {
+        $XmlText = $XmlText + $Xml
+    }
+    
+    END {
+        $XmlText = [xml]$XmlText
+        
+        if (-not $XmlText ) { return }
+        
+        $stringWriter = New-Object System.IO.StringWriter
+        
+        $settings = new-object System.Xml.XmlWriterSettings
+        $settings.Indent = $true
+        $settings.IndentChars = " " * $Indent
+        $settings.NewLineChars = "`r`n"
+        if (-not ($NoNewLineOnAttribute)) {
+            $settings.NewLineOnAttributes = $true
+        }
+        
+        $settings.OmitXmlDeclaration = $false
+        $settings.CheckCharacters = $true
 
-    $stringWriter = New-Object System.IO.StringWriter
+        $settings.NamespaceHandling = [System.Xml.NamespaceHandling]::OmitDuplicates
 
-    $writer = New-Object System.Xml.XmlTextWriter $stringWriter
-    $writer.Formatting = "indented"
-    $writer.Indentation = $indent
+        
+        $writer = [Xml.XmlWriter]::Create($stringWriter, $settings)
+        
+        $XmlText.WriteContentTo($writer)
 
-    $xml.WriteContentTo($writer)
+        $writer.Flush()
+        $StringWriter.Flush()
 
-    $writer.Flush()
-    $StringWriter.Flush()
+        Write-Output $StringWriter.ToString()
+    }
+}
 
-    Write-Output $StringWriter.ToString()
+Function Format-Json {
+    param (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Json
+    )
+    
+    BEGIN {
+        $JsonText = ""
+    }
+    
+    PROCESS {
+        $JsonText = $JsonText + $Json
+    }
+    
+    END {
+        if (-not $JsonText ) { return }
+
+        $JsonText | ConvertFrom-Json | ConvertTo-Json
+    }
 }
 
 
 Export-ModuleMember Format-Xml
+Export-ModuleMember Format-Json
