71307dfbd3f87096488eba7000341bdcff10f51a
Julian Easterling <julian@julianscorner.com>
2014-06-22 10:07:32 -0400

    updated the ISE prompt to be just one symbol instead of the longer default one with the directory in it.

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -1,7 +1,18 @@
-# PowerShell ISE Profile Script
-
 . "$(Split-Path $profile)\Load-ProfileModulesAndScripts.ps1" PowerShellISE
 . "$(Split-Path $profile)\Load-ProfileModulesAndScripts.ps1" GlobalScripts
 . "$(Split-Path $profile)\Load-ProfileModulesAndScripts.ps1" MyModules
 
-Set-Location C:\
+Set-Location $env:USERPROFILE
+
+$principal = new-object System.Security.principal.windowsprincipal($CurrentUser)
+
+if ($principal.IsInRole("Administrators")) {
+    $PromptAdmin = "#"
+} else {
+    $PromptAdmin = "$"
+}
+
+function prompt()
+{
+	"$PromptAdmin "
+}
