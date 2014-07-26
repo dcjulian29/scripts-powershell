c87f86ebcc94ea2be812f9ccd5bf615f31f39d53
Julian Easterling <julian@julianscorner.com>
2014-07-25 21:56:44 -0400

    npm function doesn't need node.exe in the command

--- GlobalScripts/20/dev-nodejs.ps1
+++ GlobalScripts/20/dev-nodejs.ps1
@@ -1,9 +1,8 @@
 $nodePath = Find-ProgramFiles 'nodejs\node.exe'
-$env:Path = "$env:Path;$($nodePath)"
 
 function npm()
 {
-  & $nodePath "$(Split-Path $nodePath)\.\node_modules\npm\bin\npm-cli.js" $args;
+  & "$(Split-Path $nodePath)\node_modules\npm\bin\npm-cli.js" $args;
 }
 
 function node()
