6365211e8c81ec45f7377d2feb37856847b19ad7
Julian Easterling <julian@julianscorner.com>
2014-12-03 20:30:07 -0500

    Updated script with new repo names

--- GlobalScripts/10/Update-ScriptsFromGitHub.ps1
+++ GlobalScripts/10/Update-ScriptsFromGitHub.ps1
@@ -1,16 +1,35 @@
-Function updateunzip($url, $filename, $repo, $destinationPath)
+Function updateunzip($repo, $destinationPath)
 {
-    (New-Object System.Net.WebClient).DownloadFile("$url/$filename", "$env:TEMP\$filename")
+    $baseUrl = "https://github.com/dcjulian29/scripts-{0}/archive/master.zip"
+    $url = $baseUrl.Replace("{0}", $repo)
+    $master = "$env:TEMP\master.zip"
+    
+    if (Test-Path $master) {
+        Remove-Item $master -Force
+    }
+    
+    (New-Object System.Net.WebClient).DownloadFile("$url", $master)
+
     $shell = New-Object -com Shell.Application
-    $shell.namespace($env:TEMP).CopyHere($shell.namespace("$env:TEMP\$filename").items(), 0x10) 
-    Copy-Item -Path "$env:TEMP\$repo\*" -Destination $destinationPath -Recurse -Force
-    Remove-Item -Path "$env:TEMP\$repo" -Recurse -Force
-    Remove-Item -Path "$env:TEMP\$filename" -Force
+    $shell.namespace($env:TEMP).CopyHere($shell.namespace($master).items(), 0x10) 
+
+    Copy-Item -Path "$env:TEMP\scripts-$repo-master\*" -Destination $destinationPath -Recurse -Force
+
+    Remove-Item -Path "$env:TEMP\scripts-$repo-master" -Recurse -Force
+    Remove-Item -Path $master -Force
 }
 
 
 Function Update-ScriptsFromGitHub {
-    updateunzip "https://github.com/dcjulian29/bin-scripts/archive" "master.zip" "bin-scripts-master" $env:SYSTEMDRIVE\tools\binaries
-    updateunzip "https://github.com/dcjulian29/dev-scripts/archive" "master.zip" "dev-scripts-master" $env:SYSTEMDRIVE\tools\development
-    updateunzip "https://github.com/dcjulian29/WindowsPowerShell/archive" "master.zip" "WindowsPowerShell-master" $env:SYSTEMDRIVE\tools\powershell
+    if (Test-Path $env:SYSTEMDRIVE\tools\binaries) {
+        updateunzip "binaries" $env:SYSTEMDRIVE\tools\binaries
+    }
+    
+    if (Test-Path $env:SYSTEMDRIVE\tools\development) {
+        updateunzip "development" $env:SYSTEMDRIVE\tools\development
+    }
+
+    if (Test-Path $env:SYSTEMDRIVE\tools\powershell) {
+        updateunzip "powershell" $env:SYSTEMDRIVE\tools\powershell
+    }
 }
