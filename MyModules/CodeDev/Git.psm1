4062c2edea7296583da740b4de3d4ed7bfab849b
Julian Easterling <julian@julianscorner.com>
2014-12-24 10:27:44 -0500

    Moved "script aliases" into a Git Module and migrated Git Ignores into it.

---
+++ MyModules/CodeDev/Git.psm1
@@ -0,0 +1,118 @@
+﻿$script:GIT_INSTALL_ROOT = Find-ProgramFiles "Git\bin"
+$script:GIT = "${script:GIT_INSTALL_ROOT}\git.exe"
+$script:DEV_TOOLS_ROOT = "${env:SYSTEMDRIVE}\tools\development"
+
+if (Test-Path "$GIT_INSTALL_ROOT") {
+    $env:Path = "$env:Path;$GIT_INSTALL_ROOT"
+
+    if (Test-Path "$($env:UserProfile)\Documents\WindowsPowerShell\Modules\posh-git") {
+        Import-Module Posh-Git
+
+        Enable-GitColors
+
+        $GitPromptSettings.BeforeText = "["
+    }
+}
+
+Function Get-GitIgnore {
+    <#
+    .Synopsis
+        Displays list of supported templates.
+    .Description
+        Command will download list of supported .gitignore file from github.
+    #>
+
+    Param (
+        [Parameter(Position = 0, Mandatory=$false, HelpMessage="Template Name")]
+        [string] $Template
+    )
+
+    $webClient = new-object Net.WebClient
+    $webClient.Headers['User-Agent']='PowerShell/$(PSVersionTable.PSVersion.Major).$(PSVersion.PSVersion.Minor)'
+
+    if ($Template) {
+        try {
+            $webClient.DownloadString("https://api.github.com/gitignore/templates/$Template") | ConvertFrom-Json | Select-Object -ExpandProperty source
+        } catch [Exception] {
+            Write-Error "Template '$Template' not found"
+        }
+    } else {
+        $webClient.DownloadString("https://api.github.com/gitignore/templates") | ConvertFrom-Json
+    }
+}
+
+Function Add-GitIgnore {
+    <#
+    .Synopsis
+        Add the requested .gitignore to current directory.
+    .Description
+        Command will create .gitignore file for specific template and put it to the current folder.
+    #>
+
+    Param (
+        [Parameter(Position = 0, Mandatory=$true, HelpMessage="Template Name")]
+        [ValidateNotNullOrEmpty()]
+        [string] $Template
+    )
+
+    $content = Get-GitIgnore $Template
+
+    if ($content) {
+        Out-File -Encoding UTF8 -Filepath .gitignore -NoClobber -InputObject $content
+    }
+}
+
+Function Backup-GitRepository {
+    & "$DEV_TOOLS_ROOT\git-backup.bat"
+}
+
+Function Remove-GitRepositoryBackup {
+    & "$DEV_TOOLS_ROOT\git-backup-remove.bat"
+}
+
+Function Push-GitRepository {
+    & "$GIT" push --all
+    & "$GIT" push --tags
+}
+
+Function Pull-GitRepository {
+    & "$GIT" pull --all
+    & "$GIT" pull --tags
+}
+
+Function Fetch-GitRepository {
+    & "$GIT" fetch --all
+}
+
+Function Get-GitRepositoryStatus {
+    & "$GIT" status
+}
+
+###################################################################################################
+
+Export-ModuleMember Get-GitIgnore
+Export-ModuleMember Add-GitIgnore
+Export-ModuleMember Backup-GitRepository
+Export-ModuleMember Remove-GitRepositoryBackup
+Export-ModuleMember Pull-GitRepository
+Export-ModuleMember Push-GitRepository
+Export-ModuleMember Fetch-GitRepository
+Export-ModuleMember Get-GitRepositoryStatus
+
+Set-Alias gb Backup-GitRepository
+Export-ModuleMember -Alias gb
+
+Set-Alias gbr Remove-GitRepositoryBackup
+Export-ModuleMember -Alias gbr
+
+Set-Alias gpull Pull-GitRepository
+Export-ModuleMember -Alias gpull
+
+Set-Alias gpush Push-GitRepository
+Export-ModuleMember -Alias gpush
+
+Set-Alias gfetch Fetch-GitRepository
+Export-ModuleMember -Alias gfetch
+
+Set-Alias gs Get-GitRepositoryStatus
+Export-ModuleMember -Alias gs
