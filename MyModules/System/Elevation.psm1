db0a7961b6d30a867f57a6853bb628c15104e6fa
Julian Easterling <julian@julianscorner.com>
2015-01-23 23:01:37 -0500

    Moved remaining admin functions to the Elevation module

--- MyModules/System/Elevation.psm1
+++ MyModules/System/Elevation.psm1
@@ -11,6 +11,82 @@ Function Test-Elevation {
     }
 }
 
+Function Invoke-ElevatedCommand {
+    $file, [string]$arguments = $args;
+    $psi = new-object System.Diagnostics.ProcessStartInfo $file;
+    $psi.Arguments = $arguments;
+    $psi.Verb = "runas";
+    [System.Diagnostics.Process]::Start($psi);
+}
+
+Function Invoke-ElevatedCommandAs {
+    $file, [string]$arguments = $args;
+    $psi = new-object System.Diagnostics.ProcessStartInfo $file;
+    $psi.Arguments = $arguments;
+    $psi.Verb = "runasuser";
+    [System.Diagnostics.Process]::Start($psi);
+}
+
+Function Invoke-ElevatedScript {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ScriptBlock] $ScriptBlock,
+        [Parameter(ValueFromPipeline = $true)]
+        $InputObject,
+        [switch] $EnableProfile
+    )
+
+    BEGIN {
+        $inputItems = New-Object System.Collections.ArrayList
+    }
+
+    PROCESS {
+        $null = $inputItems.Add($inputObject)
+    }
+
+    END {
+        $outputFile = [IO.Path]::GetTempFileName()
+        $inputFile = [IO.Path]::GetTempFileName()
+        $inputItems.ToArray() | Export-CliXml -Depth 1 $inputFile
+        $commandString = "Set-Location '$($pwd.Path)'; " +
+            "`$output = Import-CliXml '$inputFile' | " +
+            "& {" + $scriptblock.ToString() + "} 2>&1; " +
+            "Export-CliXml -Depth 1 -In `$output '$outputFile'"
+
+        $commandBytes = [System.Text.Encoding]::Unicode.GetBytes($commandString)
+        $encodedCommand = [Convert]::ToBase64String($commandBytes)
+        $commandLine = "-EncodedCommand $encodedCommand"
+
+        if (-not $EnableProfile) {
+            $commandLine = "-NoProfile {0}" -f $commandLine
+        }
+
+        $process = Start-Process -FilePath (Get-Command powershell).Definition `
+            -ArgumentList $commandLine -Verb RunAs `
+            -WindowStyle Hidden `
+            -Passthru
+
+        $process.WaitForExit()
+
+        if ((Get-Item $outputFile).Length -gt 0) {
+            Import-CliXml $outputFile
+        }
+
+        Remove-Item $outputFile
+        Remove-Item $inputFile
+    }
+}
+
 ##############################################################################
 
 Export-ModuleMember Test-Elevation
+Export-ModuleMember Invoke-ElevatedCommand
+Export-ModuleMember Invoke-ElevatedCommandAs
+Export-ModuleMember Invoke-ElevatedScript
+
+Set-Alias sudo Invoke-ElevatedCommand
+Export-ModuleMember -Alias sudo
+
+Set-Alias runas Invoke-ElevatedCommandAs
+Export-ModuleMember -Alias runas
+
