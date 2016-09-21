33ab5e70baf0224e16e1c732b4116c730623a4e7
Julian Easterling <julian@julianscorner.com>
2016-09-21 08:23:00 -0400

    Removed functions that I'm no longer using...

--- GlobalScripts/10/Set-MonitorBrightness.ps1
+++
@@ -1,6 +0,0 @@
-function Set-MonitorBrightness(
-  [Parameter(ValueFromPipeline=$True)][ValidateRange(0,100)][int]$brightness)
-{
-  $mymonitor = Get-WmiObject -Namespace root\wmi -Class WmiMonitorBrightnessMethods
-  $mymonitor.wmisetbrightness(5,$brightness)
-}
