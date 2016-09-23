1c6e177e9908401f88c84c7ad8a07505bfe02849
Julian Easterling <julian@julianscorner.com>
2016-09-23 10:18:17 -0400

    if the "development script exist, include it in the path.

--- Profile.ps1
+++ Profile.ps1
@@ -4,8 +4,7 @@
 
 $Global:CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 
-if (($env:Home).Length -eq 0) 
-{
+if (($env:Home).Length -eq 0) {
   $env:Home = $env:UserProfile
 }
 
@@ -14,13 +13,17 @@ $env:HomePath = ($env:Home).Split(":")[1]
 
 $binarydirectory = "$env:SYSTEMDRIVE\Tools\binaries"
 
-if (-not ($env:PATH).Contains($binarydirectory)) 
-{
+if (-not ($env:PATH).Contains($binarydirectory)) {
   $env:PATH = "$binarydirectory;$($env:PATH)"
+}
+
+$developmentdirectory = "$env:SYSTEMDRIVE\Tools\development"
+
+if (-not ($env:PATH).Contains($developmentdirectory)) {
+  $env:PATH = "$developmentdirectory;$($env:PATH)"
 }
 
 Remove-Item alias:wget
 Remove-Item alias:curl
 
 Set-Alias -Name go -Value gd
-
