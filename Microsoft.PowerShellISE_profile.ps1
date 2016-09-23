dcabf0fc6a907f095cdfb8c85edd72c4e56cf58d
Julian Easterling <julian@julianscorner.com>
2016-09-23 10:22:55 -0400

    Now that the modules are dynamically loading, the NuGet and ISE host should load their specific scripts/modules directly.

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -1,6 +1,14 @@
-. "$(Split-Path $profile)\Load-ProfileModulesAndScripts.ps1" PowerShellISE
-. "$(Split-Path $profile)\Load-ProfileModulesAndScripts.ps1" GlobalScripts
-. "$(Split-Path $profile)\Load-ProfileModulesAndScripts.ps1" MyModules
+$path = Get-Item "$(Split-Path $profile)\PowerShellISE"
+
+Get-ChildItem -Path $path -Filter *.ps1 -Recurse | % `
+{
+    . $_.FullName
+}
+
+Get-ChildItem -Path $path -Filter *.psm1 -Recurse | % `
+{
+    Import-Module $_.FullName -Force -DisableNameChecking
+}
 
 Set-Location $env:USERPROFILE
 
--- NuGet_profile.ps1
+++ NuGet_profile.ps1
@@ -3,7 +3,17 @@
 # nuget.exe Package Manager Console "host" is executed.
 ################################################################################
 
-. "$(Split-Path $profile)\Load-ProfileModulesAndScripts.ps1" NuGet
+$path = Get-Item "$(Split-Path $profile)\NuGet"
+
+Get-ChildItem -Path $path -Filter *.ps1 -Recurse | % `
+{
+    . $_.FullName
+}
+
+Get-ChildItem -Path $path -Filter *.psm1 -Recurse | % `
+{
+    Import-Module $_.FullName -Force -DisableNameChecking
+}
 
 function prompt()
 {
