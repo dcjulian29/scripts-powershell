b1d9e3334226b1b7dfa27c33adaa5285ac7b7734
Julian Easterling <julian@julianscorner.com>
2016-10-08 03:03:48 -0400

    Added a module for executing SQL commands.

---
+++ MyModules/SqlServer/SqlServer.psm1
@@ -0,0 +1,25 @@
+﻿Function Find-SqlBinaries {
+    $paths = @(
+        "C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Binn"
+    )
+
+    foreach ($path in $paths) {
+        if (Test-Path $path) {
+            $path
+            return
+        }
+    }
+
+}
+
+Function Invoke-SqlCommand {
+    & $(Find-SqlBinaries) $args
+}
+
+##############################################################################
+
+Export-ModuleMember Invoke-SqlCommand
+
+Set-Alias sqlcmd Invoke-SqlCommand
+
+Export-ModuleMember -Alias sqlcmd
