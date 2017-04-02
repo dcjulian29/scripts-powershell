484d1e5a8119412ea59b0ac6c8b80f02eb4db284
Julian Easterling <julian@julianscorner.com>
2017-04-01 23:08:02 -0400

    Added a couple of functions for other attibutes of the OS Install and Added a function to test for pending reboot.

--- MyModules/OperatingSystem/OperatingSystem.psm1
+++ MyModules/OperatingSystem/OperatingSystem.psm1
@@ -1,9 +1,55 @@
 Function Get-OSInstallDate {
-    Get-CimInstance Win32_OperatingSystem | select Caption, Version, InstallDate
+    (Get-CimInstance Win32_OperatingSystem).InstallDate
 }
 
+Function Get-OSVersion {
+    (Get-CimInstance Win32_OperatingSystem).Version
+}
 
+Function Get-OSRegisteredUser {
+    (Get-CimInstance Win32_OperatingSystem).RegisteredUser
+}
+
+Function Get-OSOrganization {
+    (Get-CimInstance Win32_OperatingSystem).Organization
+}
+
+Function Get-OSBuildNumber {
+    (Get-CimInstance Win32_OperatingSystem).BuildNumber
+}
+
+Function Test-PendingReboot {
+    $PendingReboot = $false
+
+    Push-Location "HKLM:\Software\Microsoft\Windows\CurrentVersion\"
+
+    if (Get-ChildItem "Component Based Servicing\RebootPending" -EA Ignore) {
+        $PendingReboot = $true
+    }
+
+    if (Get-Item "WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { 
+        $PendingReboot = $true
+    }
+
+    Pop-Location
+
+    Push-Location "HKLM:\SYSTEM\CurrentControlSet\Control"
+
+    if (Get-ItemProperty "Session Manager" -Name PendingFileRenameOperations -EA Ignore) { 
+        $PendingReboot = $true 
+    }
+
+    Pop-Location
+
+    return $PendingReboot
+}
 
 ##############################################################################
 
 Export-ModuleMember Get-OSInstallDate
+Export-ModuleMember Get-OSVersion
+Export-ModuleMember Get-OSRegisteredUser
+Export-ModuleMember Get-OSInstallDate
+Export-ModuleMember Get-OSBuildNumber
+
+Export-ModuleMember Test-PendingReboot
