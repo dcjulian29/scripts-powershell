4b55dc98c2a1c9293b30c0a037a880a13e5f1ac8
Julian Easterling <julian@julianscorner.com>
2017-04-05 22:02:17 -0400

    Updated chocolatey module to accomidate the latest version of choco.exe

--- MyModules/Chocolatey/Chocolatey.psm1
+++ MyModules/Chocolatey/Chocolatey.psm1
@@ -1,34 +1,25 @@
 ﻿Function Find-UpgradableChocolateyPackages {
     Write-Host "Examining Installed Packages..."
-    $installed = choco.exe list -localonly
-    $available = choco.exe list
-
+    $installed = choco.exe list -r -localonly
 
     foreach ($line in $installed) {
-        if ($line -match '\d+\.\d+') {
-            $package = $line.split(' ')[0]
-            $output = "Checking $package... "
-            Write-Host $output -NoNewline
+        $localVersion = $line.Split('|')[1]
+        $package = $line.Split('|')[0]
 
-            $localVersion = $line.Split(' ')[1]
+        Write-Progress -Activity "Checking $package..."
             
-            $remotePackage = $available -match "^$package\s"
+        $remotePackage = choco.exe list -r --exact $package
 
-            if ($remotePackage.Length -gt 0) {
-                $remoteVersion = ($remotePackage).Split(' ')[1]
+        if ($remotePackage.Length -gt 0) {
+            $remoteVersion = ($remotePackage).Split('|')[1]
 
-                if ($remoteVersion) {
-                    if ($localVersion -ne $remoteVersion) {
-                        Write-Host "newer version available: $remoteVersion (installed: $localVersion)" -ForegroundColor Yellow
-                    } else {
-                        Write-Host ("`b" * $output.length) -NoNewline
-                        Write-Host (" " * $output.length) -NoNewline
-                        Write-Host ("`b" * $output.length) -NoNewline
-                    }
+            if ($remoteVersion) {
+                if ($localVersion -ne $remoteVersion) {
+                    "{0,-25}Newer version available: $remoteVersion (installed: $localVersion)" -f $package
                 }
-            } else {
-                Write-Host "remote package removed." -ForegroundColor Red
             }
+        } else {
+            "{0,-25}Remote package removed." -f $package
         }
     }
 }
@@ -36,51 +27,13 @@
 Function Find-InstalledChocolateyPackages {
     $packages = (Get-ChildItem "$($env:ChocolateyInstall)\lib" | Select-Object basename).basename 
     
-    $packages | ForEach-Object { $_.split('\.')[0] } | Sort-Object -unique
+    $packages | ForEach-Object { $_.split('|')[0] } | Sort-Object -unique
 }
 
-Function Find-AvailableChocolateyPackages {
-    $installed = choco.exe list -localonly | ForEach-Object { $_.split(' ')[0] }
-    $online = choco.exe list | ForEach-Object { $_.split(' ')[0] }
-    $combined = $installed + $online | Sort-Object
-
-    $available = $combined | Group-Object | Where-Object { $_.Count -eq 1 } | Select-Object Name
-
-    return $available
-}
 
 Function Update-AllChocolateyPackages {
-    Write-Host "Examining Installed Packages..."
-    $installed = choco.exe list -localonly
-    $available = choco.exe list
-
-
-    foreach ($line in $installed) {
-        if ($line -match '\d+\.\d+') {
-            $package = $line.split(' ')[0]
-            $output = "Checking $package... "
-            Write-Host $output -NoNewline
-
-            $localVersion = $line.Split(' ')[1]
-            
-            $remotePackage = $available -match "^$package\s"
-
-            if ($remotePackage.Length -gt 0) {
-                $remoteVersion = ($remotePackage).Split(' ')[1]
-
-                if ($remoteVersion) {
-                    if ($localVersion -ne $remoteVersion) {
-                        Update-ChocolateyPackage $package
-                    } else {
-                        Write-Host ("`b" * $output.length) -NoNewline
-                        Write-Host (" " * $output.length) -NoNewline
-                        Write-Host ("`b" * $output.length) -NoNewline
-                    }
-                }
-            } else {
-                Write-Host "remote package removed." -ForegroundColor Red
-            }
-        }
+    if (Assert-Elevation) {
+        Invoke-Expression "choco.exe update all -y"
     }
 }
 
@@ -151,37 +104,6 @@ Function Add-ChocolateyToPath {
     setx.exe /m PATH $path
 }
 
-Function Purge-ObsoleteChocolateyPackages {
-    $expression = "(?<name>[^\.]+)\.(?<major>\d+)\.(?<minor>\d+)\.?(?<revision>\d+)?\.?(?<patch>\d+)?"
-    $packageDir = "${env:ChocolateyInstall}\lib"
-    $packages = Get-ChildItem -Path $packageDir -Directory `
-        | Sort-Object -Property `
-            @{Expression={[RegEx]::Match($_.Name, $expression).Groups["name"].Value}}, `
-            @{Expression={[int][RegEx]::Match($_.Name, $expression).Groups["major"].Value}}, `
-            @{Expression={[int][RegEx]::Match($_.Name, $expression).Groups["minor"].Value}}, `
-            @{Expression={[int][RegEx]::Match($_.Name, $expression).Groups["revision"].Value}}, `
-            @{Expression={[int][RegEx]::Match($_.Name, $expression).Groups["patch"].Value}} `
-
-    for ($i = 0; $i -lt $packages.Count - 1; $i++) {
-        $this = $packages[$i].Name
-        $next = $packages[$i + 1].Name
-
-        $a = $this.IndexOf('.')
-        $b = $next.IndexOf('.')
-
-        $thisName = $this.Substring(0,$a)
-        $nextName = $next.Substring(0,$b)
-
-        $thisVersion = $this.Substring($a + 1)
-        $nextVersion = $next.Substring($b + 1)
-
-        if ($thisName -eq $nextName) {
-            Write-Output "Purging $thisName : $thisVersion (< $nextVersion)"
-            Remove-Item $packages[$i].FullName -Recurse -Force
-        }
-    }
-}
-
 Function Make-ChocolateyPackage {
     Param (
         [ValidateScript({ Test-Path $(Resolve-Path $_) })]
@@ -198,13 +120,10 @@ Function Make-ChocolateyPackage {
 
 Export-ModuleMember Find-UpgradableChocolateyPackages
 Export-ModuleMember Find-InstalledChocolateyPackages
-Export-ModuleMember Find-AvailableChocolateyPackages
 Export-ModuleMember Update-AllChocolateyPackages
 Export-ModuleMember Update-ChocolateyPackage
 Export-ModuleMember Install-ChocolateyPackage
 Export-ModuleMember Uninstall-ChocolateyPackage
-Export-ModuleMember Add-ChocolateyToPath
-Export-ModuleMember Purge-ObsoleteChocolateyPackages
 
 Set-Alias chocoupdate Update-ChocolateyPackage
 Export-ModuleMember -Alias chocoupdate
