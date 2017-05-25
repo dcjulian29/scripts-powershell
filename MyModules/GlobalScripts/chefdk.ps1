221922bb974825d1f1c7ee8da9ad91a5cc062bc8
Julian Easterling <julian@julianscorner.com>
2017-05-25 09:59:58 -0400

    Not using Chef at the moment so removed it module.

--- MyModules/GlobalScripts/chefdk.ps1
+++
@@ -1,7 +0,0 @@
-$chefdk_root = "C:\opscode\chefdk"
-
-if (Test-Path "$chefdk_root\bin") {
-    & "$chefdk_root\bin\chef.bat" shell-init powershell | Out-String | Invoke-Expression
-    Import-Module "$chefdk_root\modules\chef\chef.psm1" -DisableNameChecking
-}
-    
