e9639efe4cbdfabd16fadb4b126118069c6c00e4
Julian Easterling <julian@julianscorner.com>
2017-05-25 09:59:30 -0400

    updated Editors to point to the installed location of VIM and removed the "old" Notepad++ script file in the GlobalScripts.

--- MyModules/Editors/Editors.psm1
+++ MyModules/Editors/Editors.psm1
@@ -1,19 +1,36 @@
-Function Start-VI {
-    & vim.exe $args
+$script:VIM = Find-ProgramFiles 'vim\vim80\vim.exe'
+$script:GVIM = Find-ProgramFiles 'vim\vim80\gvim.exe'
+$script:NOTEPAD = Find-ProgramFiles 'Notepad++\notepad++.exe'
+
+Function Start-VIM {
+    Start-Process -FilePath $VIM -ArgumentList $args -NoNewWindow -Wait
+}
+
+Function Start-GVIM {
+    Start-Process -FilePath $VIM -ArgumentList $args
 }
 
 Function Start-Notepad {
-    & (Find-ProgramFiles 'Notepad++\notepad++.exe') $args
+    Start-Process -FilePath $NOTEPAD -ArgumentList $args
 }
 
 ##############################################################################
 
-Export-ModuleMember Start-VI
+Export-ModuleMember Start-VIM
+Export-ModuleMember Start-GVIM
 Export-ModuleMember Start-Notepad
 
-Set-Alias vi Start-VI
+Set-Alias vi Start-VIM
 Export-ModuleMember -Alias vi
+
+Set-Alias vim Start-VIM
+Export-ModuleMember -Alias vim
+
+Set-Alias vim Start-GVIM
+Export-ModuleMember -Alias gvim
 
 Set-Alias np Start-Notepad
 Export-ModuleMember -Alias np
 
+Set-Alias notepad Start-Notepad
+Export-ModuleMember -Alias notepad
--- MyModules/GlobalScripts/10/Notepad.ps1
+++
@@ -1,7 +0,0 @@
-$notepadPath = Find-ProgramFiles 'Notepad++\notepad++.exe'
-
-if ($notepadPath)
-{
-  Set-Alias notepad $notepadPath
-  Set-Alias np $notepadPath
-}
