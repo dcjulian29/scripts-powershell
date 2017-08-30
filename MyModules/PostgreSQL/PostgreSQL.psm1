2cf130c009f505181ee351344cc57879253af234
Julian Easterling <julian@julianscorner.com>
2017-08-30 12:17:23 -0400

    PostgreSQLClient shouldn't assume to start a local instance of Postgre server.

--- MyModules/PostgreSQL/PostgreSQL.psm1
+++ MyModules/PostgreSQL/PostgreSQL.psm1
@@ -5,9 +5,6 @@ Function Start-PostgreSQLServer {
 
 Function Start-PostgreSQLClient {
     Start-Process "$(Find-ProgramFiles 'PostgreSQL\9.6\pgAdmin 4\bin')\pgAdmin4.exe"
-
-    $service = Get-Service | Where-Object { $_.Name -like "postgresql*" }
-    & sc.exe start $service
 }
 
 Function Stop-PostgreSQLServer {
@@ -22,6 +19,8 @@ Export-ModuleMember Stop-PostgreSQLServer
 
 Set-Alias postgresql-start Start-PostgreSQLServer
 Set-Alias postgresql-stop Stop-PostgreSQLServer
+Set-Alias pgadmin Start-PostgreSQLClient
 
 Export-ModuleMember -Alias postgresql-start
 Export-ModuleMember -Alias postgresql-stop
+Export-ModuleMember -Alias pgadmin
