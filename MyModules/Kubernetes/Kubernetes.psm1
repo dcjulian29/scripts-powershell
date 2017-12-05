bb3ca2d8dd76cd2d48c9866e9b3cc6bc4fa0c0ad
Julian Easterling <julian@julianscorner.com>
2017-12-05 11:03:19 -0500

    Added a module to load multiple Kubernetes profile.

---
+++ MyModules/Kubernetes/Kubernetes.psm1
@@ -0,0 +1,28 @@
+﻿Function Clear-KubernetesProfile {
+    $env:KUBECONFIG = ''
+}
+
+Function Load-KubernetesProfile {
+    param (
+        [string]$ProfileName = $(Read-Host "Enter the Kubernetes Profile")
+    )
+
+    $profileFile = Join-Path -Path "$($env:SystemDrive)/etc/kubernetes" -ChildPath "$ProfileName.yml"
+
+    if (-not (Test-Path $profileFile)) {
+        Write-Error "Kubernetes Profile does not exist!"
+    } else {
+        $env:KUBECONFIG = $ProfileFile
+    }
+}
+
+###################################################################################################
+
+Export-ModuleMember Clear-KubernetesProfile
+Export-ModuleMember Load-KubernetesProfile
+
+Set-Alias kubernetes-profile-clear Clear-KubernetesProfile
+Export-ModuleMember -Alias kubernetes-profile-clear
+
+Set-Alias kubernetes-profile-load Load-KubernetesProfile
+Export-ModuleMember -Alias kubernetes-profile-load
