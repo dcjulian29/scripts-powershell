fa67414fc88c0689e2be7e6d07f3839590f1d005
Julian Easterling <julian@julianscorner.com>
2018-03-07 09:28:51 -0500

    The "HNS Internal NIC" seems to be replaced with the "Default Switch"

--- MyModules/VirtualSwitches/VirtualSwitches.psm1
+++ MyModules/VirtualSwitches/VirtualSwitches.psm1
@@ -21,11 +21,6 @@
                 New-ItemProperty $node -Name '*NdisDeviceType' -PropertyType dword -Value 1  -ea 0
             }
 
-            if ($name -like "*vEthernet (HNS Internal NIC)*") {
-                "Found Host Network Services Internal Switch, reconfiguring as an endpoint device."
-                New-ItemProperty $node -Name '*NdisDeviceType' -PropertyType dword -Value 1  -ea 0
-            }
-
             if ($name -like "*vEthernet (Default Switch)*") {
                 "Found Hyper-V Default Switch, reconfiguring as an endpoint device."
                 New-ItemProperty $node -Name '*NdisDeviceType' -PropertyType dword -Value 1  -ea 0
