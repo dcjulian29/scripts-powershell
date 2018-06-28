ce3ae0f87c62eb4f1e1ba3dd0d1efc38710561c0
Julian Easterling <julian@julianscorner.com>
2018-06-28 15:09:18 -0400

    Added a function to automate creation of RDP shortcuts.

--- MyModules/OperatingSystem/OperatingSystem.psm1
+++ MyModules/OperatingSystem/OperatingSystem.psm1
@@ -44,6 +44,42 @@ Function Test-PendingReboot {
     return $PendingReboot
 }
 
+Function New-RemoteDesktopShortcut {
+    param (
+        [string]$Path = "$ComputerName.rdp",
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        $ComputerName,
+        [string]$UserName = "${env:USERDOMAIN}\${env:USERNAME}"
+    )
+
+    if (Test-Path $Path) {
+        $choice = Select-Item -Caption "RDP File Exists" `
+            -Message "Do you want to replace the file?" `
+            -choiceList "&Yes", "&No" -default 1
+
+        if ($choice -eq 0) {
+            Remove-Item $Path -Force -Confirm:$false
+        }
+    }
+
+    Set-Content -Path $Path -Value @"
+full address:s:$ComputerName
+redirectprinters:i:0
+redirectcomports:i:0
+redirectsmartcards:i:0
+redirectclipboard:i:1
+redirectposdevices:i:0
+username:s:$UserName
+screen mode id:i:1
+use multimon:i:0
+desktopwidth:i:1366
+desktopheight:i:768
+session bpp:i:32
+compression:i:1
+"@
+}
+
 ##############################################################################
 
 Export-ModuleMember Get-OSInstallDate
@@ -53,3 +89,5 @@ Export-ModuleMember Get-OSInstallDate
 Export-ModuleMember Get-OSBuildNumber
 
 Export-ModuleMember Test-PendingReboot
+
+Export-ModuleMember New-RemoteDesktopShortcut
