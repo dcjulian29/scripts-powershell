ad0f969172a40cd81eaed3a021d6de928facc53e
Julian Easterling <julian@julianscorner.com>
2019-06-20 13:43:33 -0400

    Updated Test Scripts file to exclude my t.ps1 test script

--- Test-Scripts.ps1
+++ Test-Scripts.ps1
@@ -5,7 +5,7 @@ Get-ChildItem -Path $source -Recurse -Force |
     Where-Object { $_.psIsContainer } |
     Where-Object { $_.FullName -notlike "*.git*" } |
     ForEach-Object { $_.FullName -replace [regex]::Escape($source), $destination } |
-    ForEach-Object { 
+    ForEach-Object {
         if (-not (Test-Path $_)) {
             New-Item -ItemType Container -Path $_ | Out-Null
         }
@@ -17,4 +17,5 @@ Get-ChildItem -Path $source -Recurse -Force |
     Where-Object { $_.Name -notlike ".gitignore" } |
     Where-Object { $_.Name -notlike "README.md" } |
     Where-Object { $_.Name -notlike "Test-Scripts.ps1" } |
+    Where-Object { $_.Name -notlike "t.ps1" } |
     Copy-Item -Force -Destination { $_.FullName -replace [regex]::Escape($source), $destination }
