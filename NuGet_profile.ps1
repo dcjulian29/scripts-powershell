ba21835b8ba275e232c8883cf8b9dafa538dbd24
Julian Easterling <julian@julianscorner.com>
2019-08-09 18:15:27 -0400

    Updated indentation of NuGet Package Manager Host

--- NuGet_profile.ps1
+++ NuGet_profile.ps1
@@ -15,7 +15,17 @@ Get-ChildItem -Path $path -Filter *.psm1 -Recurse | % `
     Import-Module $_.FullName -Force -DisableNameChecking
 }
 
-function prompt()
-{
-	"§->";
+function prompt() {
+    $realLASTEXITCODE = $LASTEXITCODE
+    $Host.UI.RawUI.BackgroundColor = $(Get-Host).UI.RawUI.BackgroundColor
+    $originalColor = $Host.UI.RawUI.ForegroundColor
+
+    Write-Host('§') -nonewline -foregroundcolor Yellow
+    Write-Host('-') -nonewline -foregroundcolor Green
+    Write-Host(">") -foregroundcolor $originalColor
+
+    $Host.UI.RawUI.ForegroundColor = $originalColor
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return "  `b"
 }
