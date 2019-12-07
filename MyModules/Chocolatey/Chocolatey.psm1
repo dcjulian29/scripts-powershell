4015aa14fded5768782070c2e3a605bd56013806
Julian Easterling <julian@julianscorner.com>
2019-12-06 20:20:33 -0500

    Changed Chocolatey module to use PSD and updated a couple of functions to work a little better and reuse code.

--- MyModules/Chocolatey/Chocolatey.psm1
+++ MyModules/Chocolatey/Chocolatey.psm1
@@ -1,16 +1,65 @@
-﻿Function Find-UpgradableChocolateyPackages {
+﻿function Find-InstalledChocolateyPackages {
+    param (
+        [switch] $NameOnly,
+        [switch] $PassThru
+    )
+
+    $output = @()
+
+    if ($NameOnly) {
+        $packages = choco.exe list -r --local --id-only
+        $line = @{Expression={$_.Name};Label="Name";width=40}
+    } else {
+        $packages = choco.exe list -r --local
+        $line = @{Expression={$_.Name};Label="Name";width=40}, `
+                @{Expression={$_.Version};Label="Installed Version";width=20}
+    }
+
+    foreach ($package in $packages) {
+        $name = $package.Split('|')[0]
+        $version = $package.Split('|')[1]
+
+        if ($NameOnly) {
+            $item = New-Object PSObject -Property @{
+                'Name' = $name
+            }
+        } else {
+            $item = New-Object PSObject -Property @{
+                'Name' = $name
+                'Version' = $version
+            }
+        }
+
+        $output += $item
+    }
+
+    if ($PassThru) {
+        return $output
+    } else {
+        $output | Format-Table $line
+    }
+}
+
+function Find-UpgradableChocolateyPackages {
+    param (
+        [switch] $PassThru
+    )
+
     Write-Host "Examining Installed Packages..."
-    $packages = choco.exe upgrade all -r -whatif
+    $packages = choco.exe upgrade all -r -whatif -y
 
     $output = @()
 
     foreach ($package in $packages) {
+        if ($package -notlike "*|*") {
+            continue
+        }
+
         $Name = $package.Split('|')[0]
         $localVersion = $package.Split('|')[1]
         $remoteVersion = $package.Split('|')[2]
 
         if ($localVersion -ne $remoteVersion) {
-            #"{0,-35}Newer version available: $remoteVersion (installed: $localVersion)" -f $Name
             $item = New-Object PSObject -Property @{
                 'Name' = $Name
                 'RemoteVersion' = $remoteVersion
@@ -21,44 +70,19 @@
         }
     }
 
-    $line = @{Expression={$_.Name};Label="Name";width=40}, `
-            @{Expression={$_.LocalVersion};Label="Installed Version";width=20}, `
-            @{Expression={$_.RemoteVersion};Label="New Version";width=20}
+    if ($PassThru) {
+        return $output
+    } else {
+        $line = @{Expression={$_.Name};Label="Name";width=40}, `
+                @{Expression={$_.LocalVersion};Label="Installed Version";width=20}, `
+                @{Expression={$_.RemoteVersion};Label="New Version";width=20}
 
-    $Output | Format-Table $line
-}
-
-Function Find-InstalledChocolateyPackages {
-    $packages = (Get-ChildItem "$($env:ChocolateyInstall)\lib" | Select-Object basename).basename 
-    
-    $packages | ForEach-Object { $_.split('|')[0] } | Sort-Object -unique
-}
-
-
-Function Update-AllChocolateyPackages {
-    if (Assert-Elevation) {
-        Invoke-Expression "choco.exe upgrade all -y"
+        $output | Format-Table $line
     }
 }
 
-Function Update-ChocolateyPackage {
-    Param (
-        [alias("ia","installArgs")][string] $installArguments,
-        [parameter(Mandatory=$true, Position=1)]
-        [string] $package
-    )
-
-    if (Assert-Elevation) {
-        if ($installArguments) {
-            $args = " -installArguments $installArguments"
-        }
-
-        Invoke-Expression "choco.exe upgrade $package$args -y"
-    }
-}
-
-Function Install-ChocolateyPackage {
-    Param (
+function Install-ChocolateyPackage {
+    param (
         [string]$version,
         [alias("ia","installArgs")][string] $installArguments,
         [parameter(Mandatory=$true, Position=1)]
@@ -78,8 +102,22 @@ Function Install-ChocolateyPackage {
     }
 }
 
-Function Uninstall-ChocolateyPackage {
-    Param (
+function New-ChocolateyPackage {
+    param (
+        [ValidateScript({ Test-Path $(Resolve-Path $_) })]
+        [string] $NuspecFile = "package.nuspec"
+    )
+
+    $options = "-Verbosity detailed -NoPackageAnalysis -NonInteractive -NoDefaultExcludes"
+
+    Invoke-Expression "nuget.exe pack ""$NuspecFile"" $options"
+}
+
+Set-Alias choco-make-package Make-ChocolateyPackage
+Set-Alias Make-ChocolateyPackage New-ChocolateyPackage
+
+function Uninstall-ChocolateyPackage {
+    param (
         [string]$version,
         [alias("ia","installArgs")][string] $installArguments,
         [parameter(Mandatory=$true, Position=1)]
@@ -99,44 +137,47 @@ Function Uninstall-ChocolateyPackage {
     }
 }
 
-Function Add-ChocolateyToPath {
-    $chocolateyPath = "C:\ProgramData\chocolatey\bin"
+function Update-AllChocolateyPackages {
+    $packages = Find-UpgradableChocolateyPackages -PassThru
 
-    $path = "${env:Path};$chocolateyPath"
+    $updatePackages = @()
 
-    $env:Path = $path
-    setx.exe /m PATH $path
+    foreach ($package in $packages) {
+        if ($package.LocalVersion -ne $package.RemoteVersion) {
+            Write-Output "  - Adding $($package.Name) to upgrade queue."
+            $updatePackages += $package.Name
+        }
+    }
+
+    if ($updatePackages.Length -gt 0) {
+        if (Assert-Elevation) {
+            foreach ($package in $updatePackages) {
+                Write-Output ""
+                Invoke-Expression "choco.exe upgrade $package -y"
+            }
+        }
+    } else {
+        Write-Output "  - All Chocolatey packages are up-to-date."
+    }
 }
 
-Function Make-ChocolateyPackage {
-    Param (
-        [ValidateScript({ Test-Path $(Resolve-Path $_) })]
-        [string] $NuspecFile = "package.nuspec"
+Set-Alias Upgrade-AllChocolateyPackage Update-AllChocolateyPackages
+
+function Update-ChocolateyPackage {
+    param (
+        [alias("ia","installArgs")][string] $installArguments,
+        [parameter(Mandatory=$true, Position=1)]
+        [string] $package
     )
 
-    $nuget = "C:\ProgramData\chocolatey\lib\NuGet.CommandLine\tools\nuget.exe"
-    $options = "-Verbosity detailed -NoPackageAnalysis -NonInteractive -NoDefaultExcludes"
+    if (Assert-Elevation) {
+        if ($installArguments) {
+            $args = " -installArguments $installArguments"
+        }
 
-    Invoke-Expression "$nuget pack ""$NuspecFile"" $options"
+        Invoke-Expression "choco.exe upgrade $package$args -y"
+    }
 }
 
-###################################################################################################
-
-Export-ModuleMember Find-UpgradableChocolateyPackages
-Export-ModuleMember Find-InstalledChocolateyPackages
-Export-ModuleMember Update-AllChocolateyPackages
-Export-ModuleMember Update-ChocolateyPackage
-Export-ModuleMember Install-ChocolateyPackage
-Export-ModuleMember Uninstall-ChocolateyPackage
-
 Set-Alias chocoupdate Update-ChocolateyPackage
-Export-ModuleMember -Alias chocoupdate
-
 Set-Alias Upgrade-ChocolateyPackage Update-ChocolateyPackage
-Export-ModuleMember -Alias Upgrade-ChocolateyPackage
-
-
-Export-ModuleMember Make-ChocolateyPackage
-
-Set-Alias choco-make-package Make-ChocolateyPackage
-Export-ModuleMember -Alias choco-make-package
