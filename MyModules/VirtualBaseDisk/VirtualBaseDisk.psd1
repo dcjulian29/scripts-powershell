0f660c139e6f5aeb5859060b44e990e09f03aa21
Julian Easterling <julian@julianscorner.com>
2019-12-18 15:19:36 -0500

    Added a new function to create a base image for DevVMs and re-did the BaseImage function to handle ISO files as well as WIM files

--- MyModules/VirtualBaseDisk/VirtualBaseDisk.psd1
+++ MyModules/VirtualBaseDisk/VirtualBaseDisk.psd1
@@ -1,14 +1,15 @@
 @{
     RootModule = 'VirtualBaseDisk.psm1'
-    ModuleVersion = '2019.9.18.1'
+    ModuleVersion = '2019.12.18.2'
     GUID = 'ed6e65e3-8813-426c-aa4c-b0373081f509'
     Author = 'Julian Easterling'
     PowerShellVersion = '3.0'
     TypesToProcess = @()
     FormatsToProcess = @()
     FunctionsToExport = @(
-        "Convert-WindowsImage"
-        "Get-WindowsImageInWIM"
+        "Get-WindowsImagesInISO"
+        "Get-WindowsImagesInWIM"
+        "New-DevBaseVhdxDisk"
         "New-BaseVhdxDisk"
     )
     AliasesToExport = @()
--- MyModules/VirtualBaseDisk/VirtualBaseDisk.psm1
+++ MyModules/VirtualBaseDisk/VirtualBaseDisk.psm1
@@ -1,4 +1,85 @@
-function Get-WindowsImageInWIM {
+function doesVhdxBlock {
+    param (
+        [string] $File,
+        [switch] $Force
+    )
+
+    if (Test-Path $File) {
+        if ($Force) {
+            Remove-Item -Path $File -Force
+        } else {
+            Write-Warning "$File already exists, Not Overwriting..."
+            return $true
+        }
+    }
+
+    return $false
+}
+
+function getWIMFileName($File) {
+    $wim = $File
+
+    if (isIsoFile($File)) {
+        $image = Get-DiskImage -ImagePath $File
+
+        if (-not $image.Attached) {
+            $image = Mount-DiskImage -ImagePath $image.ImagePath -Access ReadOnly
+
+            Start-Sleep -Seconds 1
+        }
+
+        $drive = ($image | Get-Volume).DriveLetter
+
+        $wim = "{0}:\sources\install.wim" -f $drive
+    }
+
+    return $wim
+}
+
+function isWimFile($File) {
+    if ($File.EndsWith(".wim")) {
+        return $true
+    }
+}
+
+function isIsoFile($File) {
+    if ($File.EndsWith(".iso")) {
+        return $true
+    }
+}
+
+###############################################################################
+
+function Get-WindowsImagesInISO {
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({ Test-Path $(Resolve-Path $_) })]
+        [string] $IsoFile
+    )
+
+    if (isIsoFile($IsoFile)) {
+        $image = Get-DiskImage -ImagePath $IsoFile
+
+        if (-not $image.Attached) {
+            $image = Mount-DiskImage -ImagePath $image.ImagePath -Access ReadOnly
+
+            Start-Sleep -Seconds 1
+        }
+
+        $drive = ($image | Get-Volume).DriveLetter
+
+        $wim = "{0}:\sources\install.wim" -f $drive
+
+        if (Test-Path $wim) {
+            Get-WindowsImagesInWIM $wim
+        }
+
+        $image | Dismount-DiskImage | Out-Null
+    }
+}
+
+function Get-WindowsImagesInWIM {
     param (
         [Parameter(Mandatory=$true)]
         [ValidateNotNullOrEmpty()]
@@ -6,12 +87,14 @@ function Get-WindowsImageInWIM {
         [string] $WimFile
     )
 
-    Get-WindowsImage -ImagePath $WimFile `
-    | Sort-Object ImageIndex `
-    | Select-Object `
-        @{Label="#"; Expression={$_.ImageIndex}}, `
-        @{Label="Name"; Expression={$_.ImageName}} `
-    | Format-Table -AutoSize
+    if (isWimFile($WimFile)) {
+        Get-WindowsImage -ImagePath $WimFile `
+        | Sort-Object ImageIndex `
+        | Select-Object `
+            @{Label="#"; Expression={$_.ImageIndex}}, `
+            @{Label="Name"; Expression={$_.ImageName}} `
+        | Format-Table -AutoSize
+    }
 }
 
 function New-BaseVhdxDisk {
@@ -19,26 +102,45 @@ function New-BaseVhdxDisk {
         [Parameter(Mandatory=$true)]
         [ValidateNotNullOrEmpty()]
         [ValidateScript({ Test-Path $(Resolve-Path $_) })]
-        [string] $WimFile,
-        [string] $VhdxFile,
-        $OsVersion = "2019",
-        [string] $Edition = "1",
-        [switch] $Force
+        [string] $File,
+        [string] $Index,
+        [switch] $Force,
+        [string] $Suffix
     )
 
-    if (-not $VhdxFile ) {
-        $File = "Win${OsVersion}Base.vhdx"
-    } else {
-        $File = $VhdxFile
+    if ((-not (isWimFile($File))) -and (-not (isIsoFile($File)))) {
+        throw "You must provide a WIM or ISO file!"
     }
 
-    if (-not (Test-Path "$File") -and $force) {
-        Write-Warning "$File already exists, Not Overwriting..."
+    if (-not $Index) {
+        if (isWimFile($File)) {
+            Get-WindowsImagesInWIM -WimFile $File
+        }
+
+        if (isIsoFile($File)) {
+            Get-WindowsImagesInISO -IsoFile $File
+        }
+
+        $Index = Read-Host "Enter the number for the OS you want"
+    }
+
+    if (-not $index) {
+        throw "Index is required!"
+    }
+
+    $wim = getWIMFileName($File)
+    $image = Get-WindowsImage -ImagePath $wim | Where-Object { $_.ImageIndex -eq $Index }
+
+    if (-not $image) {
+        throw "Image for the Index does not exist!"
+    }
+
+    $OsVersion = $image.ImageName -replace '[^0-9]'
+
+    $vhdx = "Win{0}Base{1}.vhdx" -f $OsVersion, $Suffix
+
+    if (doesVhdxBlock -File $vhdx -Force $Force.IsPresent) {
         return
-    }
-
-    if ((Test-Path "$File") -and $force) {
-        Remove-Item -Path "$File" -Force
     }
 
     $partition = "UEFI"
@@ -49,11 +151,40 @@ function New-BaseVhdxDisk {
 
     Push-Location $((Get-VMHost).VirtualHardDiskPath)
 
-    Write-Output "Creating a base disk using [$WimFile] for $OsVersion [$Edition] to $File..."
+    Write-Output "Creating a base disk using ""$($image.ImageName)"" to $vhdx..."
 
-    Convert-WindowsImage -SourcePath $WimFile -Edition $Edition `
-        -DiskLayout $partition  -VHDPath $file `
+    Convert-WindowsImage -SourcePath $wim -Edition $Index `
+        -DiskLayout $partition  -VHDPath $vhdx `
         -VHDFormat VHDX -SizeBytes 100GB -Verbose
 
     Pop-Location
+
+    if (isIsoFile($File)) {
+        Get-DiskImage -ImagePath $File  Dismount-DiskImage | Out-Null
+    }
+}
+
+function New-DevBaseVhdxDisk {
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({ Test-Path $(Resolve-Path $_) })]
+        [string] $File
+    )
+
+    if ((-not (isWimFile($File))) -and (-not (isIsoFile($File)))) {
+        throw "You must provide a WIM or ISO file!"
+    }
+
+    $wim = getWIMFileName($File)
+
+    $image = Get-WindowsImage -ImagePath $wim `
+        | Where-Object { $_.ImageName -eq 'Windows 10 Pro' }
+
+    if ($image) {
+        $index = $image.ImageIndex
+        New-BaseVhdxDisk -File $wim -Index $index -Suffix "Development" -Force
+    } else {
+        Write-Output "Windows 10 Pro image does not exists in that file!"
+    }
 }
