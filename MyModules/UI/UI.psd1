ac2134b52a5f38cc43bf9ffd5ce85f8f57f65991
Julian Easterling <julian@julianscorner.com>
2019-12-21 11:28:37 -0500

    Added a Read-MultiLineInput function

--- MyModules/UI/UI.psd1
+++ MyModules/UI/UI.psd1
@@ -1,6 +1,6 @@
 @{
     RootModule = 'UI.psm1'
-    ModuleVersion = '2019.9.5.1'
+    ModuleVersion = '2019.12.21.1'
     GUID = '64252dac-92cf-41fb-9ef7-94e1a42c56ac'
     Author = 'Julian Easterling'
     PowerShellVersion = '3.0'
@@ -8,6 +8,7 @@
     FormatsToProcess = @()
     FunctionsToExport = @(
         "Get-BingWallpaper"
+        "Read-MultiLineInput"
         "Select-Item"
         "Set-WindowTitle"
         "Set-BingDesktopWallpaper"
--- MyModules/UI/UI.psm1
+++ MyModules/UI/UI.psm1
@@ -45,6 +45,55 @@ function Get-BingWallpaper {
     }
 }
 
+function Read-MultiLineInput {
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Message
+    )
+
+    Add-Type -AssemblyName System.Drawing
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $label = New-Object System.Windows.Forms.Label
+    $label.Location = New-Object System.Drawing.Size(10,10)
+    $label.Size = New-Object System.Drawing.Size(280,20)
+    $label.AutoSize = $true
+    $label.Text = $Message
+
+    $textBox = New-Object System.Windows.Forms.TextBox
+    $textBox.Location = New-Object System.Drawing.Size(10,40)
+    $textBox.Size = New-Object System.Drawing.Size(575,200)
+    $textBox.AcceptsReturn = $true
+    $textBox.AcceptsTab = $false
+    $textBox.Multiline = $true
+    $textBox.ScrollBars = 'Both'
+
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Location = New-Object System.Drawing.Size(415,250)
+    $okButton.Size = New-Object System.Drawing.Size(75,25)
+    $okButton.Text = "OK"
+    $okButton.Add_Click({ $form.Tag = $textBox.Text; $form.Close() })
+
+    $form = New-Object System.Windows.Forms.Form
+    $form.Size = New-Object System.Drawing.Size(610,320)
+    $form.FormBorderStyle = 'FixedSingle'
+    $form.StartPosition = "CenterScreen"
+    $form.AutoSizeMode = 'GrowAndShrink'
+    $form.Topmost = $True
+    $form.AcceptButton = $okButton
+    $form.ShowInTaskbar = $false
+
+    $form.Controls.Add($label)
+    $form.Controls.Add($textBox)
+    $form.Controls.Add($okButton)
+
+    $form.Add_Shown({$form.Activate()})
+    $form.ShowDialog() > $null
+
+    return $form.Tag
+}
+
 function Select-Item {
     <#
         .Description
