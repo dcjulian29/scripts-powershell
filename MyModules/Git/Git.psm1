f03b32c83dd0ffd97031d12aabed137ac1a14f21
Julian Easterling <julian@julianscorner.com>
2019-12-21 23:20:38 -0500

    Alphabetized functions in Git module

--- MyModules/Git/Git.psm1
+++ MyModules/Git/Git.psm1
@@ -2,6 +2,27 @@
 $script:GIT = "${script:GIT_INSTALL_ROOT}\bin\git.exe"
 $script:GITK = "${script:GIT_INSTALL_ROOT}\cmd\gitk.exe"
 
+Function Add-GitIgnoreTemplate {
+    <#
+    .Synopsis
+        Add the requested .gitignore to current directory.
+    .Description
+        Command will create .gitignore file for specific template and put it to the current folder.
+    #>
+
+    Param (
+        [Parameter(Position = 0, Mandatory=$true, HelpMessage="Template Name")]
+        [ValidateNotNullOrEmpty()]
+        [string] $Template
+    )
+
+    $content = Get-GitIgnoreTemplate $Template
+
+    if ($content) {
+        Out-File -Encoding UTF8 -Filepath .gitignore -NoClobber -InputObject $content
+    }
+}
+
 Function Add-GitIgnoreToLocalRepository {
     param (
         [Parameter(Mandatory=$true)]
@@ -45,54 +66,6 @@ Function Add-GitIgnoreToRemoteRepository {
     }
 }
 
-Function Get-GitIgnoreTemplate {
-    <#
-    .Synopsis
-        Displays list of supported templates.
-    .Description
-        Command will download list of supported .gitignore file from github.
-    #>
-
-    Param (
-        [Parameter(Position = 0, Mandatory=$false, HelpMessage="Template Name")]
-        [string] $Template
-    )
-
-    $webClient = new-object Net.WebClient
-    $webClient.Headers['User-Agent']='PowerShell/$(PSVersionTable.PSVersion.Major).$(PSVersion.PSVersion.Minor)'
-
-    if ($Template) {
-        try {
-            $webClient.DownloadString("https://api.github.com/gitignore/templates/$Template") | ConvertFrom-Json | Select-Object -ExpandProperty source
-        } catch [Exception] {
-            Write-Error "Template '$Template' not found"
-        }
-    } else {
-        $webClient.DownloadString("https://api.github.com/gitignore/templates") | ConvertFrom-Json
-    }
-}
-
-Function Add-GitIgnoreTemplate {
-    <#
-    .Synopsis
-        Add the requested .gitignore to current directory.
-    .Description
-        Command will create .gitignore file for specific template and put it to the current folder.
-    #>
-
-    Param (
-        [Parameter(Position = 0, Mandatory=$true, HelpMessage="Template Name")]
-        [ValidateNotNullOrEmpty()]
-        [string] $Template
-    )
-
-    $content = Get-GitIgnoreTemplate $Template
-
-    if ($content) {
-        Out-File -Encoding UTF8 -Filepath .gitignore -NoClobber -InputObject $content
-    }
-}
-
 Function Backup-GitRepository {
     param (
         [ValidateNotNullorEmpty()]
@@ -123,6 +96,182 @@ Function Backup-GitRepository {
 }
 
 Set-Alias gb Backup-GitRepository
+
+Function Get-GitBranchesThatAreLocal {
+    & "$GIT" for-each-ref --sort refname --format='%(refname:short)' refs/heads
+}
+
+Function Get-GitBranchesThatAreRemote {
+    & "$GIT" for-each-ref --sort refname --format='%(refname:short)' refs/remotes
+}
+
+Function Get-GitIgnoreTemplate {
+    <#
+    .Synopsis
+        Displays list of supported templates.
+    .Description
+        Command will download list of supported .gitignore file from github.
+    #>
+
+    Param (
+        [Parameter(Position = 0, Mandatory=$false, HelpMessage="Template Name")]
+        [string] $Template
+    )
+
+    $webClient = new-object Net.WebClient
+    $webClient.Headers['User-Agent']='PowerShell/$(PSVersionTable.PSVersion.Major).$(PSVersion.PSVersion.Minor)'
+
+    if ($Template) {
+        try {
+            $webClient.DownloadString("https://api.github.com/gitignore/templates/$Template") | ConvertFrom-Json | Select-Object -ExpandProperty source
+        } catch [Exception] {
+            Write-Error "Template '$Template' not found"
+        }
+    } else {
+        $webClient.DownloadString("https://api.github.com/gitignore/templates") | ConvertFrom-Json
+    }
+}
+
+Function Get-GitRepositoryStatus {
+    & "$GIT" status
+}
+
+Set-Alias gs Get-GitRepositoryStatus
+
+Function Invoke-FetchGitRepository {
+    & "$GIT" fetch --prune --all
+}
+
+Set-Alias Fetch-GitRepository Invoke-FetchGitRepository
+Set-Alias gfetch Invoke-FetchGitRepository
+
+Function Invoke-PullGitRepository {
+    Fetch-GitRepository
+    & "$GIT" pull
+}
+
+Set-Alias Pull-GitRepository Invoke-PullGitRepository
+Set-Alias gpull Invoke-PullGitRepository
+
+Function Optimize-AllGitRepositories {
+    param (
+        [ValidateNotNullorEmpty()]
+        [ValidateScript({Test-Path $_ -PathType 'Container'})]
+        [string] $Path = $pwd
+    )
+
+    Write-Output "Cleaning all GIT repositories in this directory..."
+
+    $folders = Get-ChildItem -Path $Path | Where-Object { $_.PSIsContainer }
+
+    foreach ($folder in $folders) {
+        if (Test-Path "$(Join-Path $folder.FullName ".git")") {
+            Write-Output "== $($folder.Name)"
+            Push-Location $folder.FullName
+            & "$GIT" gc
+            Pop-Location
+            Write-Output ""
+        }
+    }
+}
+
+Set-Alias git-gc-all Optimize-AllGitRepositories
+
+Function Publish-GitRepositoryToPROD {
+    param (
+        [switch] $FromUAT,
+        [switch] $FromQA
+    )
+
+    if ($FromUAT -and $FromQA) {
+        throw "You cannot publish from both environments."
+    }
+
+    if ( -not ($FromUAT -or $FromQA)) {
+        throw "You must select an environment to publish from."
+    }
+
+    $date = [DateTime]::Now.ToString("MMMM d, yyyy ""at"" h:mm ""GMT""zzz")
+
+    & "$GIT" checkout prod
+
+    if ($FromQA) {
+        $commit = "Publish QA to Production on $date"
+        & "$GIT" merge --no-ff qa -m $commit
+    } else {
+        $commit = "Publish UAT to Production on $date"
+        & "$GIT" merge --no-ff uat -m $commit
+    }
+}
+
+Function Publish-GitRepositoryToQA {
+
+    $tag = & $GIT lasttag
+    $date = [DateTime]::Now.ToString("MMMM d, yyyy ""at"" h:mm ""GMT""zzz")
+
+    $commit = "Publish $tag to QA on $date"
+
+    & "$GIT" checkout qa
+
+    & "$GIT" merge --no-ff master -m $commit
+}
+
+Function Publish-GitRepositoryToUAT {
+    $date = [DateTime]::Now.ToString("MMMM d, yyyy ""at"" h:mm ""GMT""zzz")
+
+    $commit = "Publish QA to UAT on $date"
+
+    & "$GIT" checkout uat
+
+    & "$GIT" merge --no-ff qa -m $commit
+}
+
+Function Push-GitRepositoriesThatAreTracked {
+    # TODO: Added support for additional "remote" repositories
+    $remoteRepositories = @("origin")
+
+    foreach ($remote in $remoteRepositories) {
+        $remoteInfo = Invoke-Expression "& '$GIT' remote show origin"
+
+        foreach ($line in $remoteInfo) {
+            if ($line -match "\W*(.+) pushes to .+") {
+                "Pushing {0}/{1}..." -f $remote, $Matches[1]
+                Invoke-Expression $("& '{0}' push {1} {2}" -f $GIT, $remote, $Matches[1])
+            }
+        }
+    }
+
+    "Pushing tags..."
+    & "$GIT" push --tags
+}
+
+Set-Alias gpushall Push-GitRepositoriesThatAreTracked
+
+Function Push-GitRepository {
+    $remote = & "$GIT" remote
+    "Pushing to $remote..."
+    & "$GIT" push
+    "Pushing tags..."
+    & "$GIT" push --tags
+}
+
+Set-Alias gpush Push-GitRepository
+
+Function Remove-AllGitChanges {
+    & "$GIT" reset HEAD
+    & "$GIT" stash save --keep-index
+    & "$GIT" stash drop
+}
+
+Function Remove-GitChanges {
+    param (
+        [ValidateNotNullorEmpty()]
+        [ValidateScript({Test-Path $_})]
+        [string] $File
+    )
+
+    & "$GIT" checkout $File
+}
 
 Function Remove-GitRepositoryBackup {
     param (
@@ -156,6 +305,14 @@ Function Remove-GitRepositoryBackup {
 }
 
 Set-Alias gbr Remove-GitRepositoryBackup
+
+Function Remove-LastGitCommit {
+    if ($(& "$GIT" diff --exit-code) -and $(& "$GIT" diff --cached --exit-code)) {
+        & "$GIT" reset --soft HEAD~1
+    } else {
+        Write-Warning "Commit is already pushed... You will need to revert the changes instead."
+    }
+}
 
 Function Restore-GitRepositoryBackup {
     param (
@@ -191,106 +348,55 @@ Function Restore-GitRepositoryBackup {
     & robocopy.exe "$backup" "$Path" /MIR /Z
 }
 
-Function Push-GitRepository {
-    $remote = & "$GIT" remote
-    "Pushing to $remote..."
-    & "$GIT" push
-    "Pushing tags..."
-    & "$GIT" push --tags
-}
-
-Set-Alias gpush Push-GitRepository
-
-Function Invoke-PullGitRepository {
-    Fetch-GitRepository
-    & "$GIT" pull
-}
-
-Set-Alias Pull-GitRepository Invoke-PullGitRepository
-Set-Alias gpull Invoke-PullGitRepository
-
-Function Invoke-FetchGitRepository {
-    & "$GIT" fetch --prune --all
-}
-
-Set-Alias Fetch-GitRepository Invoke-FetchGitRepository
-Set-Alias gfetch Invoke-FetchGitRepository
-
-Function Get-GitRepositoryStatus {
-    & "$GIT" status
-}
-
-Set-Alias gs Get-GitRepositoryStatus
-
-Function Push-GitRepositoriesThatAreTracked {
-    # TODO: Added support for additional "remote" repositories
-    $remoteRepositories = @("origin")
-
-    foreach ($remote in $remoteRepositories) {
-        $remoteInfo = Invoke-Expression "& '$GIT' remote show origin"
-
-        foreach ($line in $remoteInfo) {
-            if ($line -match "\W*(.+) pushes to .+") {
-                "Pushing {0}/{1}..." -f $remote, $Matches[1]
-                Invoke-Expression $("& '{0}' push {1} {2}" -f $GIT, $remote, $Matches[1])
-            }
+Function Show-AllGitInformation {
+    Get-ChildItem -Directory | ForEach-Object {
+        if (Test-Path "$(Join-Path $_.FullName ".git")") {
+            Push-Location $_.FullName
+            Write-Output "== $($_.Name)"
+            & "$GIT" status
+            Pop-Location
+        } else {
+            Write-Output "-- $($_.Name)"
         }
-    }
 
-    "Pushing tags..."
-    & "$GIT" push --tags
+        Write-Output ""
+    }
 }
 
-Set-Alias gpushall Push-GitRepositoriesThatAreTracked
+Set-Alias status-all-projects Show-AllGitInformation
 
-Function Publish-GitRepositoryToQA {
+Function Show-GitInformation {
+    if (Test-Path "$(Join-Path $pwd.Path ".git")") {
+        Write-Output "== Remote URLs:"
+        & "$GIT" remote -v
+        Write-Output ""
 
-    $tag = & $GIT lasttag
-    $date = [DateTime]::Now.ToString("MMMM d, yyyy ""at"" h:mm ""GMT""zzz")
+        Write-Output "== Branches:"
+        & "$GIT" branch -a
+        Write-Output ""
 
-    $commit = "Publish $tag to QA on $date"
+        Write-Output "== Configuration:"
+        & "$GIT" config --local --list
+        Write-Output ""
 
-    & "$GIT" checkout qa
+        Write-Output "== Most Recent Commit"
+        & "$GIT" --no-pager log --max-count=1
+        Write-Output ""
 
-    & "$GIT" merge --no-ff master -m $commit
-}
-
-Function Publish-GitRepositoryToUAT {
-    $date = [DateTime]::Now.ToString("MMMM d, yyyy ""at"" h:mm ""GMT""zzz")
-
-    $commit = "Publish QA to UAT on $date"
-
-    & "$GIT" checkout uat
-
-    & "$GIT" merge --no-ff qa -m $commit
-}
-
-Function Publish-GitRepositoryToPROD {
-    param (
-        [switch] $FromUAT,
-        [switch] $FromQA
-    )
-
-    if ($FromUAT -and $FromQA) {
-        throw "You cannot publish from both environments."
-    }
-
-    if ( -not ($FromUAT -or $FromQA)) {
-        throw "You must select an environment to publish from."
-    }
-
-    $date = [DateTime]::Now.ToString("MMMM d, yyyy ""at"" h:mm ""GMT""zzz")
-
-    & "$GIT" checkout prod
-
-    if ($FromQA) {
-        $commit = "Publish QA to Production on $date"
-        & "$GIT" merge --no-ff qa -m $commit
+        Write-Output "Type 'git log' for more commits, or 'git show' for full commit details."
     } else {
-        $commit = "Publish UAT to Production on $date"
-        & "$GIT" merge --no-ff uat -m $commit
+        Write-Warning "Not a git repository."
     }
 }
+
+Set-Alias git-info Show-GitInformation
+
+Function Start-GitGraphicalInterface {
+    & "$GITK"
+}
+
+Set-Alias gitk Start-GitGraphicalInterface
+
 
 Function Update-AllGitRepositories {
     param (
@@ -321,108 +427,3 @@ Function Update-AllGitRepositories {
         }
     }
 }
-
-Function Optimize-AllGitRepositories {
-    param (
-        [ValidateNotNullorEmpty()]
-        [ValidateScript({Test-Path $_ -PathType 'Container'})]
-        [string] $Path = $pwd
-    )
-
-    Write-Output "Cleaning all GIT repositories in this directory..."
-
-    $folders = Get-ChildItem -Path $Path | Where-Object { $_.PSIsContainer }
-
-    foreach ($folder in $folders) {
-        if (Test-Path "$(Join-Path $folder.FullName ".git")") {
-            Write-Output "== $($folder.Name)"
-            Push-Location $folder.FullName
-            & "$GIT" gc
-            Pop-Location
-            Write-Output ""
-        }
-    }
-}
-
-Set-Alias git-gc-all Optimize-AllGitRepositories
-
-Function Get-GitBranchesThatAreLocal {
-    & "$GIT" for-each-ref --sort refname --format='%(refname:short)' refs/heads
-}
-
-Function Get-GitBranchesThatAreRemote {
-    & "$GIT" for-each-ref --sort refname --format='%(refname:short)' refs/remotes
-}
-
-Function Remove-LastGitCommit {
-    if ($(& "$GIT" diff --exit-code) -and $(& "$GIT" diff --cached --exit-code)) {
-        & "$GIT" reset --soft HEAD~1
-    } else {
-        Write-Warning "Commit is already pushed... You will need to revert the changes instead."
-    }
-}
-
-Function Start-GitGraphicalInterface {
-    & "$GITK"
-}
-
-Set-Alias gitk Start-GitGraphicalInterface
-
-Function Remove-GitChanges {
-    param (
-        [ValidateNotNullorEmpty()]
-        [ValidateScript({Test-Path $_})]
-        [string] $File
-    )
-
-    & "$GIT" checkout $File
-}
-
-Function Remove-AllGitChanges {
-    & "$GIT" reset HEAD
-    & "$GIT" stash save --keep-index
-    & "$GIT" stash drop
-}
-
-Function Show-GitInformation {
-    if (Test-Path "$(Join-Path $pwd.Path ".git")") {
-        Write-Output "== Remote URLs:"
-        & "$GIT" remote -v
-        Write-Output ""
-
-        Write-Output "== Branches:"
-        & "$GIT" branch -a
-        Write-Output ""
-
-        Write-Output "== Configuration:"
-        & "$GIT" config --local --list
-        Write-Output ""
-
-        Write-Output "== Most Recent Commit"
-        & "$GIT" --no-pager log --max-count=1
-        Write-Output ""
-
-        Write-Output "Type 'git log' for more commits, or 'git show' for full commit details."
-    } else {
-        Write-Warning "Not a git repository."
-    }
-}
-
-Set-Alias git-info Show-GitInformation
-
-Function Show-AllGitInformation {
-    Get-ChildItem -Directory | ForEach-Object {
-        if (Test-Path "$(Join-Path $_.FullName ".git")") {
-            Push-Location $_.FullName
-            Write-Output "== $($_.Name)"
-            & "$GIT" status
-            Pop-Location
-        } else {
-            Write-Output "-- $($_.Name)"
-        }
-
-        Write-Output ""
-    }
-}
-
-Set-Alias status-all-projects Show-AllGitInformation
