133c164998a5fcc0638c9c20460a4a3a8009c1f5
Julian Easterling <julian@julianscorner.com>
2019-12-25 17:10:20 -0500

    Alphabetized functions

--- MyModules/OperatingSystem/OperatingSystem.psm1
+++ MyModules/OperatingSystem/OperatingSystem.psm1
@@ -29,32 +29,6 @@ function Get-OSBuildNumber {
     (Get-CimInstance Win32_OperatingSystem).BuildNumber
 }
 
-function Test-PendingReboot {
-    $PendingReboot = $false
-
-    Push-Location "HKLM:\Software\Microsoft\Windows\CurrentVersion\"
-
-    if (Get-ChildItem "Component Based Servicing\RebootPending" -EA Ignore) {
-        $PendingReboot = $true
-    }
-
-    if (Get-Item "WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) {
-        $PendingReboot = $true
-    }
-
-    Pop-Location
-
-    Push-Location "HKLM:\SYSTEM\CurrentControlSet\Control"
-
-    if (Get-ItemProperty "Session Manager" -Name PendingFileRenameOperations -EA Ignore) {
-        $PendingReboot = $true
-    }
-
-    Pop-Location
-
-    return $PendingReboot
-}
-
 function New-RemoteDesktopShortcut {
     param (
         [string]$Path = "$ComputerName.rdp",
@@ -90,4 +64,30 @@ winposstr:s:0,1,0,24,1390,937
 session bpp:i:32
 compression:i:1
 "@
+}
+
+function Test-PendingReboot {
+    $PendingReboot = $false
+
+    Push-Location "HKLM:\Software\Microsoft\Windows\CurrentVersion\"
+
+    if (Get-ChildItem "Component Based Servicing\RebootPending" -EA Ignore) {
+        $PendingReboot = $true
+    }
+
+    if (Get-Item "WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) {
+        $PendingReboot = $true
+    }
+
+    Pop-Location
+
+    Push-Location "HKLM:\SYSTEM\CurrentControlSet\Control"
+
+    if (Get-ItemProperty "Session Manager" -Name PendingFileRenameOperations -EA Ignore) {
+        $PendingReboot = $true
+    }
+
+    Pop-Location
+
+    return $PendingReboot
 }
