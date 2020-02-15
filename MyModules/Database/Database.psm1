b961f366241b9e1c448f2a6f8ded8c3615c9551a
Julian Easterling <julian@julianscorner.com>
2020-02-15 13:19:10 -0500

    Removed PostgreSQL functions

--- MyModules/Database/Database.psm1
+++ MyModules/Database/Database.psm1
@@ -6,13 +6,6 @@ function Find-MSSqlBinaries {
         (Find-ProgramFiles 'Microsoft SQL Server\110\Tools\Binn')
 }
 
-function Find-PostgreSqlPath {
-    First-Path `
-        (Find-ProgramFiles 'PostgreSQL\10\pgAdmin 4\bin') `
-        (Find-ProgramFiles 'PostgreSQL\9.6\pgAdmin 4\bin') `
-        (Find-ProgramFiles 'PostgreSQL\9.5\pgAdmin 4\bin') 
-}
-
 ##############################################################################
 
 function Invoke-MSSqlCommand {
@@ -24,22 +17,8 @@ function Start-MSSqlServer {
     & sc.exe start MSSQLSERVER
 }
 
-function Start-PostgreSQLClient {
-    Start-Process "$(Find-PostgreSqlPath)\pgAdmin4.exe"
-}
-
-function Start-PostgreSQLServer {
-    $service = Get-Service | Where-Object { $_.Name -like "postgresql*" }
-    & sc.exe start $service
-}
-
 function Stop-MSSqlServer {
     & sc.exe stop MSSQLSERVER
-}
-
-function Stop-PostgreSQLServer {
-    $service = Get-Service | Where-Object { $_.Name -like "postgresql*" }
-    & sc.exe stop $service
 }
 
 ##############################################################################
@@ -47,22 +26,11 @@ function Stop-PostgreSQLServer {
 Export-ModuleMember Invoke-MSSqlCommand
 
 Export-ModuleMember Start-MSSqlServer
-Export-ModuleMember Start-PostgreSQLClient
-Export-ModuleMember Start-PostgreSQLServer
 
 Export-ModuleMember Stop-MSSqlServer
-Export-ModuleMember Stop-PostgreSQLServer
 
 Set-Alias mssql-start Start-MSSqlServer
 Set-Alias mssql-stop Stop-MSSqlServer
 
-Set-Alias pgadmin Start-PostgreSQLClient
-Set-Alias postgresql-start Start-PostgreSQLServer
-Set-Alias postgresql-stop Stop-PostgreSQLServer
-
 Export-ModuleMember -Alias mssql-start
 Export-ModuleMember -Alias mssql-stop
-
-Export-ModuleMember -Alias pgadmin
-Export-ModuleMember -Alias postgresql-start
-Export-ModuleMember -Alias postgresql-stop
