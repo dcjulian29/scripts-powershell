3018d11c0b6af782e543b3f564d5e99d3a2dbbd1
Julian Easterling <julian@julianscorner.com>
2020-05-01 14:11:41 -0400

    Add functions to manage SSH Known Hosts file

--- MyModules/OpenSSH/OpenSSH.psm1
+++ MyModules/OpenSSH/OpenSSH.psm1
@@ -35,6 +35,23 @@ function Add-OpenSSHServer {
     }
 }
 
+function Add-OpenSSHKnownHost {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string] $RemoteHost,
+        [Parameter(Mandatory=$true)]
+        [string] $KeyType,
+        [Parameter(Mandatory=$true)]
+        [string] $HostKey
+    )
+
+    $content = Get-Content -Path "$env:USERPROFILE/.ssh/known_hosts"
+    $line = "$RemoteHost $KeyType $HostKey"
+    $content += $line
+
+    Set-Content -Path "$env:USERPROFILE/.ssh/known_hosts" -Value $content
+}
+
 function Disable-OpenSSHServer {
      if (Test-OpenSSHServer) {
         if (Test-Elevation) {
@@ -66,6 +83,38 @@ function Get-OpenSSHDefaultShellOptions {
             (Get-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" `
                 -Name DefaultShellCommandOption).DefaultShellCommandOption
         }
+    }
+}
+
+function Get-OpenSSHKnownHosts {
+    begin {
+        $hostTable = New-Object System.Data.DataTable("KnownHosts")
+
+        "Host", "KeyType", "HostKey" | ForEach-Object {
+            $column = New-Object System.Data.DataColumn $_
+            $hostTable.Columns.Add($column)
+        }
+
+        $hostTable.Columns[0].DataType = [System.Type]::GetType("System.String[]")
+    }
+
+    process {
+        $content = Get-Content -Path "$env:USERPROFILE/.ssh/known_hosts"
+
+        foreach ($line in $content) {
+            $parts = $line.Split(' ')
+            $row = $hostTable.NewRow();
+
+            $row["Host"] = $parts[0].Split(',')
+            $row["KeyType"] = $parts[1]
+            $row["HostKey"] = $parts[2]
+
+            $hostTable.Rows.Add($row)
+        }
+    }
+
+    end {
+        return $hostTable
     }
 }
 
@@ -192,6 +241,27 @@ function Remove-OpenSSHServer {
 
             Remove-WindowsCapability -Online -Name $(getServerPackageName) | Out-Null
         }
+    }
+}
+
+function Remove-OpenSSHKnownHost {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string] $RemoteHost
+    )
+
+    $content = Get-Content -Path "$env:USERPROFILE/.ssh/known_hosts"
+    $newContent = @()
+    foreach ($line in $content) {
+        if (-not ($line -imatch ".*$([Regex]::Escape($RemoteHost)).*")) {
+            $newContent += $line
+        }
+    }
+
+    if ($content.Length -eq $newContent.Length) {
+        Write-Warning "$RemoteHost was not found in Known Hosts file."
+    } else {
+        Set-Content -Path "$env:USERPROFILE/.ssh/known_hosts" -Value $newContent
     }
 }
 
