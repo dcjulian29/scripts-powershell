51415eb579e0f952e18c10a539bdfdcda813c562
Julian Easterling <julian@julianscorner.com>
2020-05-03 02:03:29 -0400

    Updated OctopusDeploy module to use PSD

---
+++ MyModules/OctopusDeploy/OctopusDeploy.psd1
@@ -0,0 +1,35 @@
+@{
+    ModuleVersion = '2020.5.3.1'
+    GUID = 'b3764581-214b-4403-b277-e97b6ad00c51'
+    Author = 'Julian Easterling'
+    PowerShellVersion = '3.0'
+    RootModule = 'OctopusDeploy.psm1'
+    TypesToProcess = @()
+    FormatsToProcess = @()
+    FunctionsToExport = @(
+        "Clear-OctopusProfile"
+        "Find-Octo"
+        "Import-OctopusProfile"
+        "Invoke-DeployOctopusRelease"
+        "Invoke-Octo"
+        "New-OctopusPackage"
+        "New-OctopusRelease"
+        "Push-OctopusPackage"
+    )
+    AliasesToExport = @(
+        "Create-OctopusRelease"
+        "Load-OctoputProfile"
+        "octo"
+        "octo-profile-clear"
+        "octo-profile-load"
+        "octo-publish"
+        "octo-release-create"
+        "octo-release-deploy"
+        "octopus"
+        "octopus-profile-clear"
+        "octopus-profile-load"
+        "octopus-publish"
+        "octopus-release-create"
+        "octopus-release-deploy"
+    )
+}
--- MyModules/OctopusDeploy/OctopusDeploy.psm1
+++ MyModules/OctopusDeploy/OctopusDeploy.psm1
@@ -1,7 +1,7 @@
 $script:OctopusUrl = ''
 $script:OctopusApi = ''
 
-Function Get-OctopusProfileArguments {
+function getOctoProfileArguments {
     if ($script:OctopusUrl -eq '') {
         Write-Error "Octopus Profile is not loaded"
         return
@@ -10,16 +10,31 @@ Function Get-OctopusProfileArguments {
     "--server $($script:OctopusUrl) --apiKey $($script:OctopusApi)"
 }
 
-Function Clear-OctopusProfile {
+###############################################################################
+
+function Clear-OctopusProfile {
     $script:OctopusUrl = ''
     $script:OctopusApi = ''
 }
 
-Function Load-OctopusProfile {
+Set-Alias octopus-profile-clear Clear-OctopusProfile
+Set-Alias octo-profile-clear Clear-OctopusProfile
+
+function Find-Octo {
+    $octo = First-Path `
+        "$env:ChocolateyInstall\lib\OctopusTools\tools\octo.exe" `
+        "$env:ChocolateyInstall\bin\octo.exe"
+
+    if (Test-Path -Path $octo) {
+        $octo
+    }
+}
+
+function Import-OctopusProfile {
     param (
         [string]$ProfileName = $(Read-Host "Enter the Octopus Profile")
     )
-    
+
     $profileFile = Join-Path -Path "$($env:SystemDrive)/etc/octopus" -ChildPath "$ProfileName.json"
 
     if (-not (Test-Path $profileFile)) {
@@ -32,33 +47,11 @@ Function Load-OctopusProfile {
     }
 }
 
-Function Invoke-Octopus {
-    Start-Process -FilePath "$env:ChocolateyInstall\bin\octo.exe" `
-        -ArgumentList $args -NoNewWindow -Wait
-}
+Set-Alias -Name Load-OctopusProfile -Value Import-OctopusProfile
+Set-Alias -Name octo-profile-load -Value Import-OctopusProfile
+Set-Alias -Name octopus-profile-load -Value Import-OctopusProfile
 
-Function Create-OctopusRelease {
-    param(
-        [Parameter(Mandatory = $true)]
-        [String]$Project,
-        [String]$Version,
-        [String]$PackageVersion
-    )
-
-    $parameters = "$(Get-OctopusProfileArguments) --project $Project"
-
-    if ($Version) {
-        $parameters += " --version $Version"
-    }
-
-    if ($PackageVersion) {
-        $parameters += " --packageversion $PackageVersion"
-    }
-
-    Invoke-Octopus create-release $parameters
-}
-
-Function Deploy-OctopusRelease {
+function Invoke-DeployOctopusRelease {
     param(
         [Parameter(Mandatory = $true)]
         [String]$Project,
@@ -68,7 +61,7 @@ Function Deploy-OctopusRelease {
         [String]$Environment
     )
 
-    $parameters = "$(Get-OctopusProfileArguments) --project $Project"
+    $parameters = "$(getOctoProfileArguments) --project $Project"
 
     $parameters += " --releaseNumber $Version"
 
@@ -77,57 +70,63 @@ Function Deploy-OctopusRelease {
     Invoke-Octopus deploy-release --progress $parameters
 }
 
-Function Push-OctopusPackage {
+Set-Alias -Name Deploy-OctopusRelease -Value Invoke-DeployOctopusRelease
+Set-Alias -Name octo-release-deploy -Value Invoke-DeployOctopusRelease
+Set-Alias -Name octopus-release-deploy -Value Invoke-DeployOctopusRelease
+
+function Invoke-Octo {
+    Start-Process -FilePath $(Find-Octo) -ArgumentList $args -NoNewWindow -Wait
+}
+
+Set-Alias octopus Invoke-Octopus
+Set-Alias octo Invoke-Octopus
+
+function New-OctopusPackage {
+    Param (
+        [ValidateScript({ Test-Path $(Resolve-Path $_) })]
+        [string] $NuspecFile = "package.nuspec"
+    )
+
+    Invoke-Nuget pack "$NuspecFile" -NoPackageAnalysis `
+        -NonInteractive -NoDefaultExcludes -Verbosity detailed
+}
+
+function New-OctopusRelease {
+    param(
+        [Parameter(Mandatory = $true)]
+        [String]$Project,
+        [String]$Version,
+        [String]$PackageVersion
+    )
+
+    $parameters = "$(getOctoProfileArguments) --project $Project"
+
+    if ($Version) {
+        $parameters += " --version $Version"
+    }
+
+    if ($PackageVersion) {
+        $parameters += " --packageversion $PackageVersion"
+    }
+
+    Invoke-Octo create-release $parameters
+}
+
+Set-Alias -Name Create-OctopusRelease -Value New-OctopusRelease
+Set-Alias -Name octo-release-create -Value New-OctopusRelease
+Set-Alias -Name octopus-release-create -Value New-OctopusRelease
+
+function Push-OctopusPackage {
     param(
         [Parameter(Mandatory = $true)]
         [ValidateScript({Test-Path -Path $_ })]
         [String]$Package
     )
 
-    $parameters = "push $(Get-OctopusProfileArguments) --package $Package"
-    $parameters
+    $parameters = "push $(getOctoProfileArguments) --package $Package"
+
     Invoke-Octopus $parameters
 }
 
-Function Make-OctopusPackage {
-    Param (
-        [ValidateScript({ Test-Path $(Resolve-Path $_) })]
-        [string] $NuspecFile = "package.nuspec"
-    )
-
-    $nuget = "C:\ProgramData\chocolatey\lib\NuGet.CommandLine\tools\nuget.exe"
-    $options = "-NoPackageAnalysis -NonInteractive -NoDefaultExcludes"
-
-    Invoke-Expression "$nuget pack ""$NuspecFile"" $options"
-}
-
-###################################################################################################
-
-Export-ModuleMember Clear-OctopusProfile
-Export-ModuleMember Load-OctopusProfile
-Export-ModuleMember Invoke-Octopus
-Export-ModuleMember Create-OctopusRelease
-Export-ModuleMember Deploy-OctopusRelease
-Export-ModuleMember Push-OctopusPackage
-Export-ModuleMember Make-OctopusPackage
-
-Set-Alias octopus-profile-clear Clear-OctopusProfile
-Export-ModuleMember -Alias octopus-profile-clear
-
-Set-Alias octopus-profile-load Load-OctopusProfile
-Export-ModuleMember -Alias octopus-profile-load
-
-Set-Alias octopus Invoke-Octopus
-Export-ModuleMember -Alias octopus
-
-Set-Alias octo Invoke-Octopus
-Export-ModuleMember -Alias octo
-
-Set-Alias octopus-release-create Create-OctopusRelease
-Export-ModuleMember -Alias octopus-release-create
-
-Set-Alias octopus-release-deploy Deploy-OctopusRelease
-Export-ModuleMember -Alias octopus-release-deploy
-
+Set-Alias octo-publish Push-OctopusPackage
 Set-Alias octopus-publish Push-OctopusPackage
-Export-ModuleMember -Alias octopus-publish
