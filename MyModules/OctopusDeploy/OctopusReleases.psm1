03dabf5b9a1db0ebe66186d1f77208b92b6abae0
Julian Easterling <julian@julianscorner.com>
2020-05-05 11:00:15 -0400

    Added functions to get Release information from Octopus

---
+++ MyModules/OctopusDeploy/OctopusReleases.psm1
@@ -0,0 +1,61 @@
+function Get-OctopusRelease {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $ProjectName,
+        [Parameter(Mandatory = $true)]
+        [string] $Version
+    )
+
+    $releases = Get-OctopusReleases -ProjectName $ProjectName
+
+    return $releases | Where-Object { $_.Version -eq $Version}
+}
+
+function Get-OctopusReleases {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $ProjectName,
+        [int] $Skip = 0,
+        [int] $Take
+    )
+
+    $project = Get-OctopusProject -Name $ProjectName
+
+    if (-not $project) {
+        throw "Octopus project '$ProjectName' doesn't exist"
+    }
+
+    $projectId = $project.Id
+
+    if ($Take) {
+        (Invoke-OctopusApi "projects/$projectId/releases?skip=$Skip&take=$Take").Items
+    }
+
+    $releases = Invoke-OctopusApi "projects/$projectId/releases"
+
+    if (-not $releases) {
+        throw "Octopus project '$ProjectName' doesn't contain any releases"
+    }
+
+    $total = $releases.TotalResults
+
+    (Invoke-OctopusApi "projects/$projectId/releases?skip=$Skip&take=$total").Items
+}
+
+function Get-OctopusReleasesForProjectInEnvironment {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $ProjectName,
+        [Parameter(Mandatory = $true)]
+        [string] $Environment
+    )
+
+    $projectId = (Get-OctopusProject -ProjectName $ProjectName).Id
+    $environmentId = (Get-OctopusEnvironment -Name $Environment).Id
+
+    $deployments = (Invoke-OctopusApi "deployments?environments=$environmentId&skip=0&take=100000").Items
+
+    $deployments = $deployments | Where-Object { $_.ProjectId -eq $projectId }
+
+    return $deployments
+}
