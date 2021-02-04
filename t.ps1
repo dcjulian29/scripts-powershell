3cf56199afe34bfd94c3620c40710bbc27a4ff46
Julian Easterling <julian@julianscorner.com>
2021-02-03 22:54:18 -0500

    Removed the test scripts... I don't use this anymore since I have a cmdlet to import the development version of the module

--- t.ps1
+++
@@ -1,1 +0,0 @@
-Get-Content -Path "$PSScriptRoot\*" -Filter "*.psm1" | `    Out-String | Invoke-Expression###############################################################################Function-To-Test
