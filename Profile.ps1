d0f3bb68148f3a2d4a33af8d493d66fdb2ac0294
Julian Easterling <julian@julianscorner.com>
2022-01-10 18:40:33 -0500

    Fixed a typo

--- Profile.ps1
+++ Profile.ps1
@@ -5,13 +5,12 @@
 $global:CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 
 if (-not ($env:PATH).Contains("$env:SYSTEMDRIVE/tools/binaries")) {
-  if (Test Path "${env:SYSTEMDRIVE}/tools/binaries") {
+  if (Test-Path "${env:SYSTEMDRIVE}/tools/binaries") {
     $env:PATH = "${env:SYSTEMDRIVE}/tools/binaries;$($env:PATH)"
   }
 }
-
 if (-not ($env:PATH).Contains("$env:SYSTEMDRIVE/bin")) {
-  if (Test Path "${env:SYSTEMDRIVE}/bin") {
+  if (Test-Path "${env:SYSTEMDRIVE}/bin") {
     $env:PATH = "${env:SYSTEMDRIVE}/bin;$($env:PATH)"
   }
 }
