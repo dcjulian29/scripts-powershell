9a69839a7b611fd65b48c70a0439b9ac31712c00
Julian Easterling <julian@julianscorner.com>
2022-01-16 23:22:58 -0500

    refactoring the virtualbasedisk

--- Modules/VirtualBaseDisk/VirtualBaseDisk.psd1
+++ Modules/VirtualBaseDisk/VirtualBaseDisk.psd1
@@ -1,6 +1,6 @@
 @{
     RootModule = 'VirtualBaseDisk.psm1'
-    ModuleVersion = '2111.22.1'
+    ModuleVersion = '2201.16.1'
     GUID = 'ed6e65e3-8813-426c-aa4c-b0373081f509'
     Author = 'Julian Easterling'
     PowerShellVersion = '3.0'
@@ -9,9 +9,9 @@
     FunctionsToExport = @(
         "Get-WindowsImagesInISO"
         "Get-WindowsImagesInWIM"
-        "New-DevBaseVhdxDisk"
         "New-BaseVhdxDisk"
         "New-BaseServerVhdxDisks"
+        "New-DevBaseVhdxDisk"
     )
     AliasesToExport = @()
     ScriptsToProcess = @()
--- Modules/VirtualBaseDisk/VirtualBaseDisk.psm1
+++ Modules/VirtualBaseDisk/VirtualBaseDisk.psm1
@@ -1,235 +1,322 @@
 function doesVhdxBlock {
-    param (
-        [string] $File,
-        [switch] $Force
-    )
+  param (
+    [string] $File,
+    [switch] $Force
+  )
 
-    if (Test-Path $File) {
-        if ($Force) {
-            Remove-Item -Path $File -Force
-        } else {
-            Write-Warning "$File already exists, Not Overwriting..."
-            return $true
-        }
+  if (Test-Path $File) {
+    if ($Force) {
+      Remove-Item -Path $File -Force
+    } else {
+      Write-Warning "$File already exists, Not Overwriting..."
+      return $true
     }
+  }
 
-    return $false
+  return $false
 }
 
 function getWIMFileName($File) {
-    $wim = $File
+  $wim = Resolve-Path $File
 
-    if (isIsoFile($File)) {
-        $image = Get-DiskImage -ImagePath $File
+  if (isIsoFile($File)) {
+    $wim = "{0}:\sources\install.wim" -f $(mountISO $File)
+  }
 
-        if (-not $image.Attached) {
-            $image = Mount-DiskImage -ImagePath $image.ImagePath -Access ReadOnly
+  return $wim
+}
 
-            Start-Sleep -Seconds 1
-        }
+function mountISO($File) {
+  $image = Get-DiskImage -ImagePath $File
 
-        $drive = ($image | Get-Volume).DriveLetter
+  if (-not $image.Attached) {
+    $image = Mount-DiskImage -ImagePath $image.ImagePath -Access ReadOnly
+  }
 
-        $wim = "{0}:\sources\install.wim" -f $drive
-    }
+  do {
+   $drive = ($image | Get-Volume).DriveLetter
+  } while ($drive -eq "")
 
-    return $wim
+  return $drive
 }
 
 function isWimFile($File) {
-    if ($File.EndsWith(".wim")) {
-        return $true
-    }
+  return ($File.EndsWith(".wim"))
 }
 
 function isIsoFile($File) {
-    if ($File.EndsWith(".iso")) {
-        return $true
-    }
+  return ($File.EndsWith(".iso"))
 }
 
-###############################################################################
+#------------------------------------------------------------------------------
 
 function Get-WindowsImagesInISO {
-    param (
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [ValidateScript({ Test-Path $(Resolve-Path $_) })]
-        [string] $IsoFile
-    )
+  <#
+  .SYNOPSIS
+    Gets information about all Windows images in an ISO file.
+  .DESCRIPTION
+    The Get-WindowsImagesInISO cmdlet gets a list of Windows images in an ISO file.
+  .PARAMETER IsoFile
+    Specifies the location of an ISO file.
+  #>
+  param (
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({ Test-Path $(Resolve-Path $_) })]
+    [Alias("Path")]
+    [string] $IsoFile
+  )
 
-    if (isIsoFile($IsoFile)) {
-        $image = Get-DiskImage -ImagePath $IsoFile
+  if (isIsoFile($IsoFile)) {
+    $wim = (getWimFileName $IsoFile)
 
-        if (-not $image.Attached) {
-            $image = Mount-DiskImage -ImagePath $image.ImagePath -Access ReadOnly
-
-            Start-Sleep -Seconds 1
-        }
-
-        $drive = ($image | Get-Volume).DriveLetter
-
-        $wim = "{0}:\sources\install.wim" -f $drive
-
-        if (Test-Path $wim) {
-            Get-WindowsImagesInWIM $wim
-        }
-
-        $image | Dismount-DiskImage | Out-Null
+    if (Test-Path $wim) {
+      Get-WindowsImagesInWIM $wim
     }
+
+    Get-DiskImage -ImagePath $IsoFile | Dismount-DiskImage | Out-Null
+  }
 }
 
 function Get-WindowsImagesInWIM {
-    param (
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [ValidateScript({ Test-Path $(Resolve-Path $_) })]
-        [string] $WimFile
-    )
+  <#
+  .SYNOPSIS
+    Gets information about all Windows images in an WIM file.
+  .DESCRIPTION
+    The Get-WindowsImagesInWIM cmdlet gets a list of Windows images in a WIM file.
+  .PARAMETER IsoFile
+    Specifies the location of an WIM file.
+  #>
+  param (
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({ Test-Path $(Resolve-Path $_) })]
+    [Alias("Path")]
+    [string] $WimFile
+  )
 
-    if (isWimFile($WimFile)) {
-        Get-WindowsImage -ImagePath $WimFile `
-        | Sort-Object ImageIndex `
-        | Select-Object `
-            @{Label="#"; Expression={$_.ImageIndex}}, `
-            @{Label="Name"; Expression={$_.ImageName}} `
-        | Format-Table -AutoSize
-    }
+  if (isWimFile($WimFile)) {
+    Get-WindowsImage -ImagePath $WimFile `
+      | Sort-Object ImageIndex `
+      | Select-Object `
+        @{Label="#"; Expression={$_.ImageIndex}}, `
+        @{Label="Name"; Expression={$_.ImageName}} `
+      | Format-Table -AutoSize
+  }
 }
 
 function New-BaseVhdxDisk {
-    param (
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [ValidateScript({ Test-Path $(Resolve-Path $_) })]
-        [string] $File,
-        [string] $Index,
-        [switch] $Force,
-        [string] $Suffix
-    )
+  <#
+  .SYNOPSIS
+    Create a "base" VHDX from the Windows image in a WIM or ISO file.
+  .DESCRIPTION
+    The New-BaseVhdxDisk cmdlet creates a VHDX file from a Windows Image contained
+    in a WIM or ISO file.
+  .PARAMETER File
+    Specifies the location of a WIM or ISO file.
+  .PARAMETER Index
+    Specifies the index number of a Windows image in a WIM or ISO file.
+  .PARAMETER Force
+    Specifies that the target VHDX file should be overwritten if it exists.
+  .PARAMETER Suffix
+    Specifies a suffix to add to the generated VHDX file name.
+  #>
+  param (
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({ Test-Path $(Resolve-Path $_) })]
+    [Alias("Path")]
+    [string] $File,
+    [string] $Index,
+    [switch] $Force,
+    [string] $Suffix
+  )
 
-    if ((-not (isWimFile($File))) -and (-not (isIsoFile($File)))) {
-        throw "You must provide a WIM or ISO file!"
-    }
+  if ((-not (isWimFile($File))) -and (-not (isIsoFile($File)))) {
+    $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+      -Message "File provided is not a WIM or ISO file!" `
+      -ExceptionType "System.IO.FileFormatException" `
+      -ErrorId "FileFormatException" `
+      -ErrorCategory InvalidArgument `
+      -TargetObject $File))
+  }
 
-    if (-not $Index) {
-        if (isWimFile($File)) {
-            Get-WindowsImagesInWIM -WimFile $File
-        }
+  if (-not $Index) {
+    Get-WindowsImagesInWIM -WimFile $File
+    Get-WindowsImagesInISO -IsoFile $File
 
-        if (isIsoFile($File)) {
-            Get-WindowsImagesInISO -IsoFile $File
-        }
+    $Index = Read-Host "Enter the number for the OS you want:"
+  }
 
-        $Index = Read-Host "Enter the number for the OS you want"
-    }
+  if (-not $index) {
+    $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+      -Message "Index is required!" `
+      -ExceptionType "System.IndexOutOfRangeException" `
+      -ErrorId "IndexOutOfRangeException" `
+      -ErrorCategory InvalidArgument `
+      -TargetObject $Index))
+  }
 
-    if (-not $index) {
-        throw "Index is required!"
-    }
+  $wim = getWIMFileName($File)
+  $image = Get-WindowsImage -ImagePath $wim | Where-Object { $_.ImageIndex -eq $Index }
 
-    $wim = getWIMFileName($File)
-    $image = Get-WindowsImage -ImagePath $wim | Where-Object { $_.ImageIndex -eq $Index }
+  if (-not $image) {
+    $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+      -Message "Image for the Index does not exist!" `
+      -ExceptionType "System.IndexOutOfRangeException" `
+      -ErrorId "IndexOutOfRangeException" `
+      -ErrorCategory InvalidArgument `
+      -TargetObject $Index))
+  }
 
-    if (-not $image) {
-        throw "Image for the Index does not exist!"
-    }
+  $OsVersion = $image.ImageName -replace '[^0-9]'
+  $vhdx = "$((Get-VMHost).VirtualHardDiskPath)\base\Win{0}Base{1}.vhdx" -f $OsVersion, $Suffix
 
-    $OsVersion = $image.ImageName -replace '[^0-9]'
+  if (doesVhdxBlock -File $vhdx -Force $Force.IsPresent) {
+    return
+  }
 
-    $vhdx = "$((Get-VMHost).VirtualHardDiskPath)\base\Win{0}Base{1}.vhdx" -f $OsVersion, $Suffix
+  $partition = "UEFI"
 
-    if (doesVhdxBlock -File $vhdx -Force $Force.IsPresent) {
-        return
-    }
+  if ($OsVersion -like "*2008*") {
+    $partition = "BIOS"
+  }
 
-    $partition = "UEFI"
+  Push-Location "$((Get-VMHost).VirtualHardDiskPath)\base"
 
-    if ($OsVersion -like "*2008*") {
-        $partition = "BIOS"
-    }
+  Write-Output "Creating a base disk using '$($image.ImageName)' to '$vhdx'..."
 
-    Push-Location $((Get-VMHost).VirtualHardDiskPath)\base
+  . $PSScriptRoot\Convert-WindowsImage.ps1;Convert-WindowsImage -SourcePath $wim `
+    -Edition $Index -DiskLayout $partition  -VHDPath $vhdx `
+    -VHDFormat VHDX -SizeBytes 100GB -Verbose
 
-    Write-Output "Creating a base disk using ""$($image.ImageName)"" to $vhdx..."
+  Pop-Location
 
-    . $PSScriptRoot\Convert-WindowsImage.ps1
-    Convert-WindowsImage -SourcePath $wim -Edition $Index `
-        -DiskLayout $partition  -VHDPath $vhdx `
-        -VHDFormat VHDX -SizeBytes 100GB -Verbose
-
-    Pop-Location
-
-    if (isIsoFile($File)) {
-        Get-DiskImage -ImagePath $File  Dismount-DiskImage | Out-Null
-    }
-}
-
-function New-DevBaseVhdxDisk {
-    param (
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [ValidateScript({ Test-Path $(Resolve-Path $_) })]
-        [string] $File,
-        [int] $OSVersion = 11
-    )
-
-    if ((-not (isWimFile($File))) -and (-not (isIsoFile($File)))) {
-        throw "You must provide a WIM or ISO file!"
-    }
-
-    $wim = getWIMFileName($File)
-
-    $image = Get-WindowsImage -ImagePath $wim `
-        | Where-Object { $_.ImageName -eq "Windows $OSVersion Pro" }
-
-    if ($image) {
-        $index = $image.ImageIndex
-
-        $build = (dism /Get-WimInfo /WimFile:D:\sources\install.wim /index:$index `
-            | select-string Version)[1] -Replace('Version : ', '')
-
-        New-BaseVhdxDisk -File $wim -Index $index -Suffix "Insider-$Build"
-    } else {
-        Write-Output "Windows $OSVersion Pro image does not exists in that file!"
-    }
+  if (isIsoFile($File)) {
+    Get-DiskImage -ImagePath $File | Dismount-DiskImage | Out-Null
+  }
 }
 
 function New-BaseServerVhdxDisks {
-    param (
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        $OSVersion,
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [ValidateScript({ Test-Path $(Resolve-Path $_) })]
-        [string] $File,
-        [switch] $Force
-    )
+  <#
+  .SYNOPSIS
+    Create two "base" VHDX from a Windows Server image in a WIM or ISO file.
+  .DESCRIPTION
+    The New-BaseServerVhdxDisk cmdlet creates two VHDX files from a Windows Server Image
+    contained in a WIM or ISO file. One VHDX is the "Desktop Experience" and the other is
+    the "Core" style.
+  .PARAMETER OsVersion
+    Specifies the version of the operating system in the WIM or ISO file.
+  .PARAMETER File
+    Specifies the location of a WIM or ISO file.
+  .PARAMETER Force
+    Specifies that the target VHDX files should be overwritten if it exists.
+  .PARAMETER EvalIso
+    Specifies that the WIM or ISO is from an Evaluation Media source.
+  #>
+  param (
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [int] $OSVersion,
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({ Test-Path $(Resolve-Path $_) })]
+    [string] $File,
+    [switch] $Force,
+    [switch] $EvalIso
+  )
 
-    if ((-not (isWimFile($File))) -and (-not (isIsoFile($File)))) {
-        throw "You must provide a WIM or ISO file!"
-    }
+  if ((-not (isWimFile($File))) -and (-not (isIsoFile($File)))) {
+    $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+      -Message "File provided is not a WIM or ISO file!" `
+      -ExceptionType "System.IO.FileFormatException" `
+      -ErrorId "FileFormatException" `
+      -ErrorCategory InvalidArgument `
+      -TargetObject $File))
+  }
 
-    $wim = getWIMFileName($File)
+  $wim = getWIMFileName($File)
+  $eval = ""
 
-    $desktopImage = Get-WindowsImage -ImagePath $wim `
-        | Where-Object { $_.ImageName -eq "Windows Server $OSVersion Standard Evaluation (Desktop Experience)" }
+  if ($EvalIso) {
+    $eval = " Evaluation"
+  }
 
-    $coreImage = Get-WindowsImage -ImagePath $wim `
-        | Where-Object { $_.ImageName -eq "Windows Server $OSVersion Standard Evaluation" }
+  $baseName = "Windows Server $OSVersion Standard$eval"
 
-    if (-not $desktopImage) {
-        throw "'Windows Server $OSVersion Standard Evaluation (Desktop Experience)' image does not exists in '$wim'!"
-    }
+  $desktopImage = Get-WindowsImage -ImagePath $wim `
+    | Where-Object { $_.ImageName -eq "$baseName (Desktop Experience)" }
 
-    if (-not $coreImage) {
-        throw "'Windows Server $OSVersion Standard Evaluation' image does not exists in '$wim'!"
-    }
+  $coreImage = Get-WindowsImage -ImagePath $wim `
+    | Where-Object { $_.ImageName -eq $baseName }
 
-    $desktopIndex = $desktopImage.ImageIndex
-    New-BaseVhdxDisk -File $wim -Index $desktopIndex -Force:$Force.IsPresent
+  if (-not $desktopImage) {
+    $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+      -Message "'$baseName (Desktop Experience)' image does not exists in '$wim'!" `
+      -ExceptionType "System.IO.FileFormatException" `
+      -ErrorId "FileFormatException" `
+      -ErrorCategory InvalidArgument `
+      -TargetObject $File))
+  }
 
-    $coreIndex = $coreImage.ImageIndex
-    New-BaseVhdxDisk -File $wim -Index $coreIndex -Suffix "Core" -Force:$Force.IsPresent
+  if (-not $coreImage) {
+    $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+      -Message "'$baseName' image does not exists in '$wim'!" `
+      -ExceptionType "System.IO.FileFormatException" `
+      -ErrorId "FileFormatException" `
+      -ErrorCategory InvalidArgument `
+      -TargetObject $File))
+  }
+
+  $desktopIndex = $desktopImage.ImageIndex
+  New-BaseVhdxDisk -File $wim -Index $desktopIndex -Force:$Force.IsPresent
+
+  $coreIndex = $coreImage.ImageIndex
+  New-BaseVhdxDisk -File $wim -Index $coreIndex -Suffix "Core" -Force:$Force.IsPresent
+}
+
+function New-DevBaseVhdxDisk {
+  <#
+  .SYNOPSIS
+    Create a "base" VHDX from the Windows Insiders image in a WIM or ISO file.
+  .DESCRIPTION
+    The New-DevBaseVhdxDisk cmdlet creates a VHDX file from a Windows Insiders Image contained
+    in a WIM or ISO file.
+  .PARAMETER File
+    Specifies the location of a Windows Insiders WIM or ISO file.
+  .PARAMETER OsVersion
+    Specifies the version of the operating system in the WIM or ISO file.
+  #>
+  param (
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({ Test-Path $(Resolve-Path $_) })]
+    [string] $File,
+    [int] $OSVersion = 11
+  )
+
+  if ((-not (isWimFile($File))) -and (-not (isIsoFile($File)))) {
+    $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+      -Message "File provided is not a WIM or ISO file!" `
+      -ExceptionType "System.IO.FileFormatException" `
+      -ErrorId "FileFormatException" `
+      -ErrorCategory InvalidArgument `
+      -TargetObject $File))
+  }
+
+  $wim = getWIMFileName($File)
+
+  $image = Get-WindowsImage -ImagePath $wim `
+    | Where-Object { $_.ImageName -eq "Windows $OSVersion Pro" }
+
+  if ($image) {
+    $index = $image.ImageIndex
+
+    $build = (dism /Get-WimInfo /WimFile:$wim /index:$index `
+      | Select-String Version)[1] -Replace('Version : ', '')
+
+    New-BaseVhdxDisk -File $wim -Index $index -Suffix "Insider-$Build"
+  } else {
+    Write-Output "Windows $OSVersion Pro image does not exists in that file!"
+  }
 }
