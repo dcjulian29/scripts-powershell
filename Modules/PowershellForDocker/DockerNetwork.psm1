34f467500780a1e691364099ff2990734aa5959c
Julian Easterling <julian@julianscorner.com>
2022-01-26 13:21:55 -0500

    Changed indentation of script files

--- Modules/PowershellForDocker/DockerNetwork.psm1
+++ Modules/PowershellForDocker/DockerNetwork.psm1
@@ -1,123 +1,127 @@
 function Connect-DockerNetwork {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string] $NetworkName,
-        [Parameter(Mandatory = $true)]
-        [string] $ContainerName,
-        [string] $IPv4,
-        [string] $IPv6,
-        [string[]] $Aliases
-    )
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [string] $NetworkName,
+    [Parameter(Mandatory = $true)]
+    [string] $ContainerName,
+    [string] $IPv4,
+    [string] $IPv6,
+    [string[]] $Aliases
+  )
 
-    $options = ""
-    if ($IPv4) {
-        $options += " --ip $IPv4"
+  $options = ""
+  if ($IPv4) {
+    $options += " --ip $IPv4"
+  }
+
+  if ($IPv6) {
+    $options += " --ip6 $IPv6"
+  }
+
+  if ($Aliases) {
+    foreach ($alias in $Aliases) {
+      $options += " --alias $alias"
     }
+  }
 
-    if ($IPv6) {
-        $options += " --ip6 $IPv6"
-    }
+  $params = "network connect$options $NetworkName $ContainerName"
 
-    if ($Aliases) {
-        foreach ($alias in $Aliases) {
-            $options += " --alias $alias"
-        }
-    }
-
-    $params = "network connect$options $NetworkName $ContainerName"
-
-    Invoke-Docker $params
+  Invoke-Docker $params
 }
 
 function Disconnect-DockerNetwork {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string] $NetworkName,
-        [Parameter(Mandatory = $true)]
-        [string] $ContainerName,
-        [switch] $Force
-    )
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [string] $NetworkName,
+    [Parameter(Mandatory = $true)]
+    [string] $ContainerName,
+    [switch] $Force
+  )
 
-    $params = "network disconnect"
+  $params = "network disconnect"
 
-    if ($Force) {
-        $params += " --force"
-    }
+  if ($Force) {
+    $params += " --force"
+  }
 
-    $params += " $NetworkName $ContainerName"
+  $params += " $NetworkName $ContainerName"
 
-    Invoke-Docker $params
+  Invoke-Docker $params
 }
 
 function Get-DockerNetwork {
-    [CmdletBinding()]
-    param (
-        [string] $Name
-    )
+  [CmdletBinding()]
+  param (
+    [string] $Name
+  )
 
-    if ($Name.Length -eq 0) {
-        $net = Invoke-Docker network ls --no-trunc
-        $list = @()
+  if ($Name.Length -eq 0) {
+    $net = Invoke-Docker network ls --no-trunc
+    $list = @()
 
-        foreach ($line in $net) {
-            if ($line.StartsWith("NETWORK")) {
-                continue
-            }
+    foreach ($line in $net) {
+      if ($line.StartsWith("NETWORK")) {
+        continue
+      }
 
-            $result = $line | Select-String -Pattern '(\S+)\s\s+(\S+)\s+(\S+)\s+(\S+)'
+      $result = $line | Select-String -Pattern '(\S+)\s\s+(\S+)\s+(\S+)\s+(\S+)'
 
-            $network = New-Object -TypeName PSObject
+      $network = New-Object -TypeName PSObject
 
-            $network | Add-Member -MemberType NoteProperty -Name Id -Value $result.Matches.Groups[1].Value
-            $network | Add-Member -MemberType NoteProperty -Name Name -Value $result.Matches.Groups[2].Value
-            $network | Add-Member -MemberType NoteProperty -Name Driver -Value $result.Matches.Groups[3].Value
-            $network | Add-Member -MemberType NoteProperty -Name Scope -Value $result.Matches.Groups[4].Value
+      $network | Add-Member -MemberType NoteProperty -Name Id `
+        -Value $result.Matches.Groups[1].Value
+      $network | Add-Member -MemberType NoteProperty -Name Name `
+        -Value $result.Matches.Groups[2].Value
+      $network | Add-Member -MemberType NoteProperty -Name Driver `
+        -Value $result.Matches.Groups[3].Value
+      $network | Add-Member -MemberType NoteProperty -Name Scope `
+        -Value $result.Matches.Groups[4].Value
 
-            $list += $network
-        }
-
-        return $list
-    } else {
-        $json = Invoke-Docker network inspect $Name
-
-        return $json | ConvertFrom-Json
+      $list += $network
     }
+
+    return $list
+  } else {
+    $json = Invoke-Docker network inspect $Name
+
+    return $json | ConvertFrom-Json
+  }
 }
 
 function New-DockerNetwork {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string] $Name
-    )
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [string] $Name
+  )
 
-    Invoke-Docker network create $Name
+  Invoke-Docker network create $Name
 }
 
 function Optimize-DockerNetwork {
-    param (
-        [switch] $Force
-    )
+  param (
+    [switch] $Force
+  )
 
-    $params = "network prune"
+  $params = "network prune"
 
-    if ($Force) {
-        $params += " --force"
-    }
+  if ($Force) {
+    $params += " --force"
+  }
 
-    Invoke-Docker $params
+  Invoke-Docker $params
 }
 
 Set-Alias -Name Prune-DockerNetwork -Value Optimize-DockerNetwork
 
 function Remove-DockerNetwork {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string] $Name
-    )
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [string] $Name
+  )
 
-    Invoke-Docker network rm $Name
+  Invoke-Docker network rm $Name
 }
--- Modules/PowershellForDocker/DockerVolume.psm1
+++ Modules/PowershellForDocker/DockerVolume.psm1
@@ -1,102 +1,104 @@
 function Get-DockerVolume {
-    [CmdletBinding()]
-    param (
-        [string] $Name
-    )
+  [CmdletBinding()]
+  param (
+    [string] $Name
+  )
 
-    if ($Name.Length -eq 0) {
-        $net = Invoke-Docker volume ls
-        $list = @()
+  if ($Name.Length -eq 0) {
+    $net = Invoke-Docker volume ls
+    $list = @()
 
-        foreach ($line in $net) {
-            if ($line.StartsWith("DRIVER")) {
-                continue
-            }
+    foreach ($line in $net) {
+      if ($line.StartsWith("DRIVER")) {
+        continue
+      }
 
-            $result = $line | Select-String -Pattern '(\S+)\s+(\S+)'
+      $result = $line | Select-String -Pattern '(\S+)\s+(\S+)'
 
-            $volume = New-Object -TypeName PSObject
+      $volume = New-Object -TypeName PSObject
 
-            $volume | Add-Member -MemberType NoteProperty -Name Name -Value $result.Matches.Groups[2].Value
-            $volume | Add-Member -MemberType NoteProperty -Name Driver -Value $result.Matches.Groups[1].Value
+      $volume | Add-Member -MemberType NoteProperty -Name Name `
+        -Value $result.Matches.Groups[2].Value
+      $volume | Add-Member -MemberType NoteProperty -Name Driver `
+        -Value $result.Matches.Groups[1].Value
 
-            $list += $volume
-        }
-
-        return $list
-    } else {
-        $json = Invoke-Docker volume inspect $Name
-
-        return $json | ConvertFrom-Json
+      $list += $volume
     }
+
+    return $list
+  } else {
+    $json = Invoke-Docker volume inspect $Name
+
+    return $json | ConvertFrom-Json
+  }
 }
 
 function New-DockerNfsVolume {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string] $Name,
-        [Parameter(Mandatory = $true)]
-        [string] $Server,
-        [Parameter(Mandatory = $true)]
-        [string] $Path
-    )
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [string] $Name,
+    [Parameter(Mandatory = $true)]
+    [string] $Server,
+    [Parameter(Mandatory = $true)]
+    [string] $Path
+  )
 
-    $options = @(
-        "type=nfs"
-        "o=addr=$Server,rw"
-        "device=:$Path"
-    )
+  $options = @(
+    "type=nfs"
+    "o=addr=$Server,rw"
+    "device=:$Path"
+  )
 
-    New-DockerVolume -Name $Name -Driver "local" -DriverOptions $options
+  New-DockerVolume -Name $Name -Driver "local" -DriverOptions $options
 }
 
 function New-DockerVolume {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string] $Name,
-        [string] $Driver,
-        [string[]] $DriverOptions
-    )
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [string] $Name,
+    [string] $Driver,
+    [string[]] $DriverOptions
+  )
 
-    $options = ""
+  $options = ""
 
-    if ($Driver) {
-        $options = " --driver $Driver"
+  if ($Driver) {
+    $options = " --driver $Driver"
+  }
+
+  if ($DriverOptions) {
+    foreach ($option in $DriverOptions) {
+      $options += " --opt $option"
     }
+  }
 
-    if ($DriverOptions) {
-        foreach ($option in $DriverOptions) {
-            $options += " --opt $option"
-        }
-    }
-
-    Invoke-Docker volume create$options $Name
+  Invoke-Docker volume create$options $Name
 }
 
 function Optimize-DockerVolume {
-    param (
-        [switch] $Force
-    )
+  param (
+    [switch] $Force
+  )
 
-    $params = "volume prune"
+  $params = "volume prune"
 
-    if ($Force) {
-        $params += " --force"
-    }
+  if ($Force) {
+    $params += " --force"
+  }
 
-    Invoke-Docker $params
+  Invoke-Docker $params
 }
 
 Set-Alias -Name Prune-DockerVolume -Value Optimize-DockerVolume
 
 function Remove-DockerVolume {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string] $Name
-    )
+ [CmdletBinding()]
+ param (
+  [Parameter(Mandatory = $true)]
+  [string] $Name
+ )
 
-    Invoke-Docker volume rm $Name
+ Invoke-Docker volume rm $Name
 }
