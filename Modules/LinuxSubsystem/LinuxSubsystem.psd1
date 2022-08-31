c1062ba93eb6b8e05ddcbda9fbd9f61a855a603d
Julian Easterling <julian@julianscorner.com>
2022-08-31 19:24:33 -0400

    Removed Kali... its better to use Live CD or VM

--- Modules/LinuxSubsystem/LinuxSubsystem.psd1
+++ Modules/LinuxSubsystem/LinuxSubsystem.psd1
@@ -1,6 +1,6 @@
 @{
     RootModule = 'LinuxSubsystem.psm1'
-    ModuleVersion = '2112.9.1'
+    ModuleVersion = '2208.31.1'
     GUID = '64252dac-92cf-41fb-9ef7-94e1a42c56ac'
     Author = 'Julian Easterling'
     PowerShellVersion = '3.0'
@@ -9,15 +9,11 @@
     FunctionsToExport = @(
         "Disable-LinuxSubsystem"
         "Enable-LinuxSubsystem"
-        "Install-KaliLinux"
         "Restart-LinuxSubsystem"
-        "Start-KaliLinux"
         "Start-UbuntuLinux"
         "Test-LinuxSubsystem"
-        "Uninstall-KaliLinux"
     )
     AliasesToExport = @(
-        "kali"
         "ubuntu"
     )
 }
--- Modules/LinuxSubsystem/LinuxSubsystem.psm1
+++ Modules/LinuxSubsystem/LinuxSubsystem.psm1
@@ -12,76 +12,11 @@ function Enable-LinuxSubsystem {
   }
 }
 
-function Install-KaliLinux {
-  if (-not (Test-LinuxSubsystem)) {
-    Write-Warning "You must reboot before using the Linux Subsystem..."
-
-    return
-  }
-
-  if (Test-Path $env:SYSTEMDRIVE\Kali) {
-    Write-Warning "Kali Linux is already installed..."
-
-    return
-  }
-
-  if (Test-Path $env:TEMP\kali.appx) {
-      Remove-Item -Path $env:TEMP\kali.appx -Force | Out-Null
-  }
-
-  if (Test-Path $env:TEMP\kali.zip) {
-      Remove-Item -Path $env:TEMP\kali.zip -Force | Out-Null
-  }
-
-  Invoke-WebRequest -Uri https://aka.ms/wsl-kali-linux -OutFile $env:TEMP\Kali.appx -UseBasicParsing
-
-  Rename-Item $env:TEMP\Kali.appx $env:TEMP\Kali.zip
-
-  New-Item -Type Directory -Path $env:SYSTEMDRIVE\Kali | Out-Null
-
-  if (Test-Path $env:TEMP\Kali) {
-      Remove-Item -Path $env:TEMP\Kali -Recurse -Force | Out-Null
-  }
-
-  New-Item -Type Directory -Path $env:TEMP\Kali | Out-Null
-
-  Expand-Archive $env:TEMP\Kali.zip $env:TEMP\Kali
-
-  Rename-Item -Path (Get-ChildItem -Filter "DistroL*x64.appx" -Path $env:TEMP\Kali).FullName `
-    -NewName $env:TEMP\Kali\Kali.zip
-
-  Expand-Archive $env:TEMP\Kali\Kali.zip $env:SYSTEMDRIVE\Kali
-
-  Set-Content $env:SYSTEMDRIVE\Kali\desktop.ini @"
-  [.ShellClassInfo]
-  IconResource=$env:SYSTEMDRIVE\Kali\kali.exe,0
-"@
-
-  attrib +S +H $env:SYSTEMDRIVE\Kali\desktop.ini
-  attrib +S $env:SYSTEMDRIVE\Kali
-
-  Remove-Item -Path $env:TEMP\Kali -Recurse -Force | Out-Null
-  Remove-Item -Path $env:TEMP\kali.zip -Force | Out-Null
-
-  Write-Output "Kali Linux has been installed...  Starting final install."
-
-  Start-Process -FilePath $env:SYSTEMDRIVE\Kali\Kali.exe -ArgumentList "install --root" `
-      -NoNewWindow -Wait
-}
-
 function Restart-LinuxSubsystem {
   if (Test-LinuxSubsystem) {
     Get-Service LxssManager | Restart-Service
   }
 }
-
-function Start-KaliLinux {
-  if (Test-Path $env:SYSTEMDRIVE\Kali\kali.exe) {
-    Start-Process -FilePath $env:SYSTEMDRIVE\Kali\kali.exe -NoNewWindow -Wait
-  }
-}
-
-Set-Alias kali Start-KaliLinux
 
 function Start-UbuntuLinux {
   $ubuntu = (Get-ChildItem -Path "${env:SYSTEMDRIVE}\Ubuntu" `
@@ -100,10 +35,3 @@ function Test-LinuxSubsystem {
   return (Get-WindowsOptionalFeature -online | Where-Object { $_.FeatureName -eq 'Microsoft-Windows-Subsystem-Linux' }).State
 }
 
-function Uninstall-KaliLinux {
-  wslconfig.exe /u MyDistribution  #Kali
-
-  if (Test-Path $env:SYSTEMDRIVE\Kali) {
-    Remove-Item -Path $env:SYSTEMDRIVE\Kali -Recurse -Force | Out-Null
-  }
-}
