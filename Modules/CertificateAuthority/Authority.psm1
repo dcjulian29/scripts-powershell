8b1c3743f5bd6e438d8a6fcf7a29485a0725351c
Julian Easterling <julian@julianscorner.com>
2022-09-27 15:52:22 -0400

    Moved function to Authority nested module

--- Modules/CertificateAuthority/Authority.psm1
+++ Modules/CertificateAuthority/Authority.psm1
@@ -629,3 +629,30 @@ function Revoke-OpenSslSubordinateAuthority {
 
   Pop-Location
 }
+
+function Test-OpenSslCertificateAuthority {
+  [CmdletBinding()]
+  [Alias("ca-test")]
+  param (
+    [Parameter(Position = 0)]
+    [ValidateScript({ Test-Path $(Resolve-Path $_) })]
+    [string] $Path = ($PWD.Path),
+    [switch] $Root,
+    [switch] $Subordinate
+  )
+
+  $result = Test-Path "$Path/.openssl_ca"
+
+  if ($result) {
+    if ($root) {
+      return (Get-Content .openssl_ca | Select-String ".type=root").Matches.Count -gt 0
+    }
+
+    if ($Subordinate) {
+      return (Get-Content .openssl_ca | Select-String ".type=subordinate").Matches.Count -gt 0
+    }
+  }
+
+  return $result
+}
+
