db08fbcee24e7bb35330882bd7807a8c7fae380f
Julian Easterling <julian@julianscorner.com>
2022-10-12 13:08:45 -0400

    Cleaned up the module

--- Modules/PowershellForVirtualBox/PowershellForVirtualBox.psd1
+++ Modules/PowershellForVirtualBox/PowershellForVirtualBox.psd1
@@ -1,30 +1,30 @@
 @{
-    RootModule = 'VirtualBox.psm1'
-    ModuleVersion = '2103.13.1'
-    GUID = 'c66d09da-ef6d-411b-8708-9526b68533fa'
-    Author = 'Julian Easterling'
-    PowerShellVersion = '3.0'
+    RootModule = "VirtualBox.psm1"
+    ModuleVersion = "2110.11.1"
+    GUID = "c66d09da-ef6d-411b-8708-9526b68533fa"
+    Author = "Julian Easterling"
+    PowerShellVersion = "3.0"
     TypesToProcess = @()
     FormatsToProcess = @()
     NestedModules = @()
     FunctionsToExport = @(
-        'Get-VirtualBoxMachine'
-        'Find-VirtualBox'
-        'Invoke-VirtualBox'
-        'Start-VirtualBoxMachine'
-        'Stop-VirtualBoxMachine'
-        'Suspend-VirtualBoxMachine'
+        "Get-VirtualBoxMachine"
+        "Find-VirtualBox"
+        "Invoke-VirtualBox"
+        "Start-VirtualBoxMachine"
+        "Stop-VirtualBoxMachine"
+        "Suspend-VirtualBoxMachine"
     )
     AliasesToExport = @(
-        'Get-VBoxMachine'
-        'gvbm'
-        'Pause-VirtualBoxMachine'
-        'Pause-VBoxMachine'
-        'Resume-VirtualBoxMachine'
-        'Resume-VBoxMachine'
-        'Start-VBoxMachine'
-        'Stop-VBoxMachine'
-        'Suspend-VBoxMachine'
-        'vbox'
+        "Get-VBoxMachine"
+        "gvbm"
+        "Pause-VirtualBoxMachine"
+        "Pause-VBoxMachine"
+        "Resume-VirtualBoxMachine"
+        "Resume-VBoxMachine"
+        "Start-VBoxMachine"
+        "Stop-VBoxMachine"
+        "Suspend-VBoxMachine"
+        "vbox"
     )
 }
--- Modules/PowershellForVirtualBox/VirtualBox.psm1
+++ Modules/PowershellForVirtualBox/VirtualBox.psm1
@@ -1,143 +1,138 @@
-function findMachine {
-    param (
-        [string]$Name
-    )
+function apiVersion {
+  return $(getVbox).apiVersion
+}
 
-    $machine = $null
+function findMachine($Name) {
+  $machine = $null
+  try { $machine = $(getVbox).FindMachine($Name) } catch {}
+  return $machine
+}
 
-    try {
-        $machine = $(getVbox).FindMachine($Name)
-    } catch {}
-
-    return $machine
+function getSession {
+  return New-Object -ComObject "VirtualBox.Session"
 }
 
 function getVbox {
-    return New-Object -ComObject "VirtualBox.VirtualBox"
+  return New-Object -ComObject "VirtualBox.VirtualBox"
 }
 
-function stateAsNumber {
-    param (
-        [string]$State
-    )
-
-    switch ($State) {
-        "Stopped"                { return  1 }
-        "Saved"                  { return  2 }
-        "Teleported"             { return  3 }
-        "Aborted"                { return  4 }
-        "Running"                { return  5 }
-        "Paused"                 { return  6 }
-        "Stuck"                  { return  7 }
-        "Snapshotting"           { return  8 }
-        "Starting"               { return  9 }
-        "Stopping"               { return 10 }
-        "Restoring"              { return 11 }
-        "TeleportingPausedVM"    { return 12 }
-        "TeleportingIn"          { return 13 }
-        "FaultTolerantSync"      { return 14 }
-        "DeletingSnapshotOnline" { return 15 }
-        "DeletingSnapshot"       { return 16 }
-        "SettingUp"              { return 17 }
-    }
+function stateAsNumber($State) {
+  switch ($State) {
+    "Stopped"                { return  1 }
+    "PoweredOff"             { return  1 }
+    "Saved"                  { return  2 }
+    "Teleported"             { return  3 }
+    "Aborted"                { return  4 }
+    "AbortedSaved"           { return  5 }
+    "Running"                { return  6 }
+    "Paused"                 { return  7 }
+    "Stuck"                  { return  8 }
+    "Teleporting"            { return  9 }
+    "LiveSnapshotting"       { return 10 }
+    "Starting"               { return 11 }
+    "Stopping"               { return 12 }
+    "Saving"                 { return 13 }
+    "Restoring"              { return 14 }
+    "FaultTolerantSync"      { return 14 }
+    "TeleportingPausedVM"    { return 15 }
+    "TeleportingIn"          { return 16 }
+    "DeletingSnapshotOnline" { return 17 }
+    "DeletingSnapshotPaused" { return 18 }
+    "OnlineSnapshotting"     { return 19 }
+    "RestoringSnapshot"      { return 20 }
+    "DeletingSnapshot"       { return 21 }
+    "SettingUp"              { return 22 }
+    "Snapshotting"           { return 23 }
+    "FirstOnline"            { return  6 }
+    "LastOnline"             { return 19 }
+    "FirstTransient"         { return  9 }
+    "LastTransient"          { return 23 }
+  }
 }
 
-function stateAsText {
-    param (
-        [int]$State
-    )
-
-    switch ($State) {
-         1 { return "Stopped" }
-         2 { return "Saved" }
-         3 { return "Teleported" }
-         4 { return "Aborted" }
-         5 { return "Running" }
-         6 { return "Paused" }
-         7 { return "Stuck" }
-         8 { return "Snapshotting" }
-         9 { return "Starting" }
-        10 { return "Stopping" }
-        11 { return "Restoring" }
-        12 { return "TeleportingPausedVM" }
-        13 { return "TeleportingIn" }
-        14 { return "FaultTolerantSync" }
-        15 { return "DeletingSnapshotOnline" }
-        16 { return "DeletingSnapshot" }
-        17 { return "SettingUp" }
-
-        default { return $State }
-    }
+function stateAsText($State) {
+  switch ($State) {
+     1 { return "PoweredOff" }
+     2 { return "Saved" }
+     3 { return "Teleported" }
+     4 { return "Aborted" }
+     5 { return "AbortedSaved" }
+     6 { return "Running" }
+     7 { return "Paused" }
+     8 { return "Stuck" }
+     9 { return "Teleporting" }
+    10 { return "LiveSnapshotting" }
+    11 { return "Starting" }
+    12 { return "Stopping" }
+    13 { return "Saving" }
+    14 { return "Restoring" }
+    15 { return "TeleportingPausedVM" }
+    16 { return "TeleportingIn" }
+    17 { return "DeletingSnapshotOnline" }
+    18 { return "DeletingSnapshotPaused" }
+    19 { return "OnlineSnapshotting"}
+    20 { return "RestoringSnapshot" }
+    21 { return "DeletingSnapshot" }
+    22 { return "SettingUp" }
+    23 { return "Snapshotting" }
+  }
 }
 
 #------------------------------------------------------------------------------
 
 function Get-VirtualBoxMachine {
-    [CmdletBinding(DefaultParameterSetName="All")]
-    param(
-        [Parameter(Position=0, ParameterSetName="Named")]
-        [string[]]$Name,
-        [Parameter(ParameterSetName="All")]
-        [switch]$All,
-        [Parameter(ParameterSetName="All")]
-        [ValidateSet("Stopped","Running","Saved","Teleported","Aborted",
-            "Paused","Stuck","Snapshotting","Starting","Stopping",
-            "Restoring","TeleportingPausedVM","TeleportingIn","FaultTolerantSync",
-            "DeletingSnapshotOnline","DeletingSnapshot","SettingUp")]
-        [string]$State = "Running"
-    )
+  [CmdletBinding()]
+  [Alias("Get-VBoxMachine","gvbm")]
+  param(
+    [Parameter(Position=0)]
+    [string[]] $Name,
+    [ValidateSet("Stopped", "Running", "Saved", "Teleported", "Aborted",
+      "Paused", "Stuck", "Snapshotting", "Starting", "Stopping",
+      "Restoring", "TeleportingPausedVM", "TeleportingIn", "FaultTolerantSync",
+      "DeletingSnapshotOnline", "DeletingSnapshot", "SettingUp")]
+    [Parameter(Position=1)]
+    [string] $State
+  )
 
-    $vbox = getVbox
-    $machines = @()
-    $virtuals = @()
+  $machines = $(getVbox).Machines
+  $virtuals = @()
 
-    if ($PSCmdlet.ParameterSetName -eq "Named") {
-        foreach ($machine in $Name) {
-            $vm = findMachine($machine)
+  if ($State.Length -gt 0) {
+    $machines = $machines | Where-Object { $_.State -eq $(stateAsNumber($State)) }
+  }
 
-            if ($vm) {
-                $machines += $vm
-            } else {
-                Write-Warning "Could not find a registered machine named '$machine'. Names are Case Sensitive."
-            }
-        }
-    } else {
-        if ($All) {
-            $machines = $vbox.Machines
-        } else {
-            $machines = $vbox.Machines | Where-Object { $_.State -eq $(stateAsNumber($State)) }
-        }
+  if ($Name.Count -gt 0) {
+    $machines = $machines | Where-Object { $_.Name -in $Name }
+  }
+
+  if ($machines) {
+    foreach ($machine in $machines) {
+      $virtuals += New-Object -TypeName PSObject -Property @{
+        Name = $machine.name
+        State = $(stateAsText($machine.State))
+        Description = $machine.description
+        ID = $machine.ID
+        OS = $machine.OSTypeID
+        CPU = $machine.CPUCount
+        MemoryMB = $machine.MemorySize
+      }
     }
+  }
 
-    if ($machines) {
-        foreach ($machine in $machines) {
-            $virtuals += New-Object -TypeName PSObject -Property @{
-                Name = $machine.name
-                State = $(stateAsText($machine.State))
-                Description = $machine.description
-                ID = $machine.ID
-                OS = $machine.OSTypeID
-                CPU = $machine.CPUCount
-                MemoryMB = $machine.MemorySize
-            }
-        }
-    }
+  $machines = $null
 
-    return $virtuals
+  return $virtuals
 }
 
-Set-Alias -Name Get-VBoxMachine -Value Get-VirtualBoxMachine
-Set-Alias -Name gvbm -Value Get-VirtualBoxMachine
-
 function Find-VirtualBox {
-    $vbox = (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' `
+    $vb = (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' `
         -Name "VBOX_MSI_INSTALL_PATH").VBOX_MSI_INSTALL_PATH + "VBoxManage.exe"
 
-    if (-not (Test-Path $vbox)) {
-        $vbox = $null
+    if (-not (Test-Path $vb)) {
+        $vb = $null
     }
 
-    return $vbox
+    return $vb
 }
 
 function Invoke-VirtualBox {
@@ -147,132 +142,129 @@ function Invoke-VirtualBox {
 Set-Alias -Name "vbox" -Value Invoke-VirtualBox
 
 function Start-VirtualBoxMachine {
-    [CmdletBinding(SupportsShouldProcess = $True)]
-    param (
-        [Parameter(Position = 0, Mandatory = $true, HelpMessage = "Enter a virtual machine name",
-                   ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [ValidateNotNullorEmpty()]
-        [Alias("Id")]
-        [string[]]$Name,
-        [switch]$Headless
-    )
+  [CmdletBinding(SupportsShouldProcess = $True)]
+  [Alias("Start-VBoxMachine", "Resume-VirtualBoxMachine", "Resume-VBoxMachine")]
+  param (
+    [Parameter(Position = 0, Mandatory = $true, HelpMessage = "Enter a virtual machine name",
+               ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [ValidateNotNullorEmpty()]
+    [Alias("Id")]
+    [string[]]$Name,
+    [switch]$Headless
+  )
 
-    begin { }
+  foreach ($item in $Name) {
+    $machine = findMachine($item)
 
-    process {
-        foreach ($item in $Name) {
-            $machine = findMachine($item)
+    if ($machine) {
+      Write-Verbose "Starting $($machine.Name)..."
+      if ($PScmdlet.ShouldProcess($machine.Name)) {
+        if ($machine.State -in @(1, 2, 6)) {
+          if ($Headless) {
+            $progress = $machine.LaunchVMProcess($(getSession), "headless", [string[]]@())
+          } else {
+            $progress = $machine.LaunchVMProcess($(getSession), "gui", [string[]]@())
+          }
 
-            if ($machine) {
-                Write-Output "Starting $($machine.Name)..."
-                if ($PScmdlet.ShouldProcess($machine.Name)) {
-                    if ($machine.State -lt 5) {
-                        $session = New-Object -ComObject "VirtualBox.Session"
-                        if ($Headless) {
-                            $progress = $machine.LaunchVMProcess($session, "headless", [string[]]@())
-                        } else {
-                            $progress = $machine.LaunchVMProcess($session, "gui", [string[]]@())
-                        }
-
-                        while ($progress.Completed -eq 0) {
-                            Start-Sleep -Seconds 1
-                        }
-                    } else {
-                        Write-Warning "Cannot start '$($machine.Name)' since it is already running."
-                    }
-                }
-            } else {
-                Write-Warning "Failed to find virtual machine '$Name'"
-            }
+          while ($progress.Completed -eq 0) {
+            Start-Sleep -Seconds 1
+          }
+        } else {
+          $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+            -Message "Cannot start '$($machine.Name)', current state: '$(stateAsText $machine.State)'" `
+            -ExceptionType "System.InvalidOperationException" `
+            -ErrorId "System.InvalidOperation" `
+            -ErrorCategory "InvalidOperation"))
         }
+      }
+    } else {
+      $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+        -Message "Failed to find virtual machine named '$Name'. Names are case sensitive." `
+        -ExceptionType "System.InvalidOperationException" `
+        -ErrorId "System.InvalidOperation" `
+        -ErrorCategory "InvalidOperation"))
     }
-
-    end { }
+  }
 }
-
-Set-Alias -Name Start-VBoxMachine -Value Start-VirtualBoxMachine
-Set-Alias -Name Resume-VirtualBoxMachine -Value Start-VirtualBoxMachine
-Set-Alias -Name Resume-VBoxMachine -Value Start-VirtualBoxMachine
 
 function Stop-VirtualBoxMachine {
-    [CmdletBinding(SupportsShouldProcess = $True)]
-    param (
-        [Parameter(Position = 0, Mandatory = $true, HelpMessage = "Enter a virtual machine name",
-                   ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [ValidateNotNullorEmpty()]
-        [Alias("Id")]
-        [string[]]$Name
-    )
+  [CmdletBinding(SupportsShouldProcess = $True)]
+  [Alias("Stop-VBoxMachine")]
+  param (
+    [Parameter(Position = 0, Mandatory = $true, HelpMessage = "Enter a virtual machine name",
+                ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [ValidateNotNullorEmpty()]
+    [Alias("Id")]
+    [string[]]$Name
+  )
 
-    begin { }
+  foreach ($item in $Name) {
+    $machine = findMachine($item)
 
-    process {
-        foreach ($item in $Name) {
-            $machine = findMachine($item)
-
-            if ($machine) {
-                Write-Output "Stopping $($machine.Name)..."
-                if ($PScmdlet.ShouldProcess($machine.Name)) {
-                    if ($machine.State -eq 5) {
-                        $session = New-Object -ComObject "VirtualBox.Session"
-                        $machine.LockMachine($session, 1)
-                        $session.Console.PowerButton()
-                    } else {
-                        Write-Warning "Cannot stop '$($machine.Name)' since it is not running."
-                    }
-                }
-            } else {
-                Write-Warning "Failed to find virtual machine '$Name'"
-            }
+    if ($machine) {
+      Write-Verbose "Stopping $($machine.Name)..."
+      if ($PScmdlet.ShouldProcess($machine.Name)) {
+        if ($machine.State -eq 6) {
+          $session = getSession
+          $machine.LockMachine($session, 1)
+          $session.Console.PowerButton()
+        } else {
+          $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+            -Message "Cannot stop '$($machine.Name)', current state: '$(stateAsText $machine.State)'" `
+            -ExceptionType "System.InvalidOperationException" `
+            -ErrorId "System.InvalidOperation" `
+            -ErrorCategory "InvalidOperation"))
         }
+      }
+    } else {
+      $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+        -Message "Failed to find virtual machine named '$Name'. Names are case sensitive." `
+        -ExceptionType "System.InvalidOperationException" `
+        -ErrorId "System.InvalidOperation" `
+        -ErrorCategory "InvalidOperation"))
     }
-
-    end { }
+  }
 }
-
-Set-Alias -Name Stop-VBoxMachine -Value Stop-VirtualBoxMachine
 
 function Suspend-VirtualBoxMachine {
-    [CmdletBinding(SupportsShouldProcess = $True)]
-    param (
-        [Parameter(Position = 0, Mandatory = $true, HelpMessage = "Enter a virtual machine name",
-                   ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [ValidateNotNullorEmpty()]
-        [Alias("Id")]
-        [string[]]$Name
-    )
+  [CmdletBinding(SupportsShouldProcess = $True)]
+  [Alias("Suspend-VBoxMachine", "Pause-VirtualBoxMachine", "Pause-VBoxMachine")]
+  param (
+    [Parameter(Position = 0, Mandatory = $true, HelpMessage = "Enter a virtual machine name",
+               ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [ValidateNotNullorEmpty()]
+    [Alias("Id")]
+    [string[]]$Name
+  )
 
-    begin { }
+  foreach ($item in $Name) {
+    $machine = findMachine($item)
 
-    process {
-        foreach ($item in $Name) {
-            $machine = findMachine($item)
+    if ($machine) {
+      Write-Verbose "Suspending $($machine.Name)..."
+      if ($PScmdlet.ShouldProcess($machine.Name)) {
+        if (($machine.State -eq 4) -or ($machine.State -eq 5)) {
+          $session = New-Object -ComObject "VirtualBox.Session"
+          $machine.LockMachine($session, 1)
+          $progress = $session.Machine.SaveState()
 
-            if ($machine) {
-                Write-Output "Suspending $($machine.Name)..."
-                if ($PScmdlet.ShouldProcess($machine.Name)) {
-                    if (($machine.State -eq 4) -or ($machine.State -eq 5)) {
-                        $session = New-Object -ComObject "VirtualBox.Session"
-                        $machine.LockMachine($session, 1)
-                        $progress = $session.Machine.SaveState()
-
-                        while ($progress.Completed -eq 0) {
-                            Start-Sleep -Seconds 1
-                        }
-
-                    } else {
-                        Write-Warning "Cannot save the execution state. '$($machine.Name)' is not running."
-                    }
-                }
-            } else {
-                Write-Warning "Failed to find virtual machine '$Name'"
-            }
+          while ($progress.Completed -eq 0) {
+            Start-Sleep -Seconds 1
+          }
+        } else {
+          $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+            -Message "Cannot save the execution state of '$($machine.Name)', current state: '$(stateAsText $machine.State)'" `
+            -ExceptionType "System.InvalidOperationException" `
+            -ErrorId "System.InvalidOperation" `
+            -ErrorCategory "InvalidOperation"))
         }
+      }
+    } else {
+      $PSCmdlet.ThrowTerminatingError((New-ErrorRecord `
+        -Message "Failed to find virtual machine named '$Name'. Names are case sensitive." `
+        -ExceptionType "System.InvalidOperationException" `
+        -ErrorId "System.InvalidOperation" `
+        -ErrorCategory "InvalidOperation"))
     }
-
-    end { }
+  }
 }
-
-Set-Alias -Name Suspend-VBoxMachine -Value Suspend-VirtualBoxMachine
-Set-Alias -Name Pause-VirtualBoxMachine -Value Suspend-VirtualBoxMachine
-Set-Alias -Name Pause-VBoxMachine -Value Suspend-VirtualBoxMachine
