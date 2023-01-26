e8e17c3b7f8e7f940d4304e236062ae03c70cbd5
Julian Easterling <julian@julianscorner.com>
2023-01-25 23:12:02 -0500

    Added export and import functions and a function to get installed WSL distributions

--- Modules/LinuxSubsystem/LinuxSubsystem.psd1
+++ Modules/LinuxSubsystem/LinuxSubsystem.psd1
@@ -1,6 +1,6 @@
 @{
     RootModule = 'LinuxSubsystem.psm1'
-    ModuleVersion = '2209.25.1'
+    ModuleVersion = '2301.25.1'
     GUID = '64252dac-92cf-41fb-9ef7-94e1a42c56ac'
     Author = 'Julian Easterling'
     PowerShellVersion = '3.0'
@@ -9,12 +9,12 @@
     FunctionsToExport = @(
         "Disable-LinuxSubsystem"
         "Enable-LinuxSubsystem"
+        "Export-LinuxSubsystemDistribution"
+        "Get-LinuxSubsystemDistribution"
         "Get-LinuxSubsystemParentProcess"
+        "Import-LinuxSubsystemDistribution"
         "Restart-LinuxSubsystem"
-        "Start-UbuntuLinux"
+        "Set-LinuxSubsystemDistribution"
         "Test-LinuxSubsystem"
-    )
-    AliasesToExport = @(
-        "ubuntu"
     )
 }
--- Modules/LinuxSubsystem/LinuxSubsystem.psm1
+++ Modules/LinuxSubsystem/LinuxSubsystem.psm1
@@ -1,30 +1,111 @@
 function Disable-LinuxSubsystem {
-  if (-not (Get-WindowsOptionalFeature -online | Where-Object { $_.FeatureName -eq 'Microsoft-Windows-Subsystem-Linux' }).State) {
+  [CmdletBinding()]
+  param ( )
+
+  if (-not (Test-LinuxSubsystem)) {
     Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
   }
 }
 
 function Enable-LinuxSubsystem {
-  if (-not (Get-WindowsOptionalFeature -online | Where-Object { $_.FeatureName -eq 'Microsoft-Windows-Subsystem-Linux' }).State) {
+  [CmdletBinding()]
+  param ( )
+
+  if (-not (Test-LinuxSubsystem)) {
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 
     Write-Warning "You must reboot before using the Linux Subsystem..."
   }
 }
 
-function Get-LinuxSubsystemParentProcess {
-    $id = tasklist /svc /fi "imagename eq svchost.exe" `
-      | Select-String ".*\s(\d+)\sLxssManager" `
-      | ForEach-Object { "$($_.Matches.Groups[1])" }
+function Export-LinuxSubsystemDistribution {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [Alias("DistributionName", "DistroName")]
+    [string] $Name,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Path
+  )
 
-    if ($id) {
-      return $id
-    } else {
-      return $null
+  if (Test-Path -Path $Path -PathType Container) {
+    $Path = Join-Path -Path $Path -ChildPath "$Name.tar"
+  }
+
+  wsl.exe --export $Name $Path
+}
+
+function Get-LinuxSubsystemDistribution {
+  [CmdletBinding()]
+  param (
+    [string] $Name,
+    [switch] $Running
+   )
+
+  $results = @()
+  $m = (wsl.exe --list --verbose) -replace "`0" -split "`n" `
+    | Select-String -Pattern '(\*|\s)\s([\S]+)\s+([\S]+)\s+([\S]+)' -AllMatches
+
+  $m | ForEach-Object {
+    if ($_.Matches.Groups[2].Value -ne 'NAME') {
+      if ($_.Matches.Groups[1].Value -eq '*') {
+        $default = $true
+      } else {
+        $default = $false
+      }
+
+      $results += [PSCustomObject]@{
+        Name = $_.Matches.Groups[2].Value
+        State = $_.Matches.Groups[3].Value
+        Version = $_.Matches.Groups[4].Value
+        Default = $default
+      }
     }
+  }
+
+  if ($Name.Length -gt 0) {
+    $results = $results | Where-Object { $_.Name -like "*$Name*" }
+  }
+
+  if ($Running) {
+    $results = $results | Where-Object { $_.State -eq "Running" }
+  }
+
+  return $results
+}
+
+function Get-LinuxSubsystemParentProcess {
+  [CmdletBinding()]
+  param ( )
+
+  $id = tasklist /svc /fi "imagename eq svchost.exe" `
+    | Select-String ".*\s(\d+)\sLxssManager" `
+    | ForEach-Object { "$($_.Matches.Groups[1])" }
+
+  if ($id.Length -lt 2) { $id = $null }
+
+  return $id
+}
+
+function Import-LinuxSubsystemDistribution {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [Alias("DistributionName", "DistroName")]
+    [string] $Name,
+    [Parameter(Mandatory = $true)]
+    [ValidateScript({ Test-Path -Path $(Resolve-Path $_) -PathType Leaf })]
+    [string] $Path
+  )
+
+  wsl.exe --import $Name $(Resolve-Path $Path)
 }
 
 function Restart-LinuxSubsystem {
+  [CmdletBinding()]
+  param ( )
+
   if (Test-LinuxSubsystem) {
     Get-Service LxssManager | Stop-Service
 
@@ -38,19 +119,55 @@ function Restart-LinuxSubsystem {
   }
 }
 
-function Start-UbuntuLinux {
-  $ubuntu = (Get-ChildItem -Path "${env:SYSTEMDRIVE}\Ubuntu" `
-    | Where-Object { $_.Name -match "^Ubuntu\d{4}.exe" } `
-    | Sort-Object Name -Descending `
-    | Select-Object -First 1).FullName
+function Set-LinuxSubsystemDistribution {
+  [CmdletBinding(DefaultParameterSetName = "default")]
+  param (
+    [parameter(ParameterSetName = "default", Mandatory = $true)]
+    [parameter(ParameterSetName = "user", Mandatory = $true)]
+    [parameter(ParameterSetName = "version", Mandatory = $true)]
+    [Alias("DistributionName", "DistroName")]
+    [string] $Name,
 
-  if ($ubuntu) {
-    Start-Process -FilePath $ubuntu -NoNewWindow -Wait
+    [parameter(ParameterSetName = "user", Mandatory = $true)]
+    [string] $User,
+
+    [parameter(ParameterSetName = "version", Mandatory = $true)]
+    [string] $Version,
+
+    [parameter(ParameterSetName = "default", Mandatory = $true)]
+    [switch] $Default
+  )
+
+  switch ($PSCmdlet.ParameterSetName) {
+    "user" {
+      Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName `
+        | Where-Object -Property DistributionName -eq $Name `
+        | Set-ItemProperty -Name DefaultUid `
+          -Value ((wsl.exe -d $Name -u $User -e id -u) | Out-String)
+    }
+
+    "version" {
+      wsl.exe --set-version $Version $Name
+    }
+
+    "default" {
+      wsl.exe -d $Name --set-default
+    }
   }
 }
 
-Set-Alias ubuntu Start-UbuntuLinux
+function Stop-LinuxSubsystemDistribution {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [Alias("DistributionName", "DistroName")]
+    [string] $Name
+  )
+
+  wsl.exe -d $Name --terminate
+}
 
 function Test-LinuxSubsystem {
-  return (Get-WindowsOptionalFeature -online | Where-Object { $_.FeatureName -eq 'Microsoft-Windows-Subsystem-Linux' }).State
+  return (Get-WindowsOptionalFeature -online `
+    | Where-Object { $_.FeatureName -eq 'Microsoft-Windows-Subsystem-Linux' }).State
 }
