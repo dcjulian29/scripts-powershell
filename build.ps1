6a3637297fdfe6397cd7ad1da2846a5b52e0b420
Julian Easterling <julian@julianscorner.com>
2023-03-26 01:48:47 -0400

    Added GH action to publish module packages

--- build.ps1
+++ build.ps1
@@ -5,46 +5,30 @@ trap [System.Exception] {
 
 $ErrorActionPreference = "Stop"
 $baseDir = (Resolve-Path $("$PSScriptRoot")).Path
-$modulesDir = Join-Path -Path $baseDir -ChildPath Modules
+$toolDir = Join-Path -Path $baseDir -ChildPath ".tools"
 
-if ($null -eq ${env:NuGetApi}) {
-  $profileFile = Join-Path -Path "$($env:SystemDrive)/etc/nuget" -ChildPath "powershell.json"
+if (Get-Command -Name "nuget") {
+  $nuget = (Get-Command -Name "nuget").Source
+} else {
+  $nuget = Join-Path -Path $toolDir -ChildPath "nuget.exe"
 
-  $json = Get-Content -Raw -Path $profileFile | ConvertFrom-Json
-
-  $env:NuGetUrl = $json.Url
-  $env:NuGetApi = $json.Api
-}
-
-#------------------------------------------------------------------------------
-
-Push-Location $baseDir
-
-$TOOLS_DIR = Join-Path $baseDir "tools"
-$NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
-$NUGET_URL = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-
-if (-not (Test-Path $NUGET_EXE)) {
-  Write-Output "Trying to find nuget.exe in PATH..."
-  $existingPaths = $Env:Path -Split ';' `
-    | Where-Object { (![string]::IsNullOrEmpty($_)) -and (Test-Path $_ -PathType Container) }
-  $NUGET_EXE_IN_PATH = Get-ChildItem -Path $existingPaths -Filter "nuget.exe" | Select-Object -First 1
-
-  if (($null -ne $NUGET_EXE_IN_PATH) -and (Test-Path $NUGET_EXE_IN_PATH.FullName)) {
-    Write-Output "Found in PATH at $($NUGET_EXE_IN_PATH.FullName)."
-    $NUGET_EXE = $NUGET_EXE_IN_PATH.FullName
-  } else {
-    if (-not (Test-Path $TOOLS_DIR)) {
-      Write-Output "Creating tools directory..."
-      New-Item -Path $TOOLS_DIR -Type Directory | Out-Null
+  if (-not (Test-Path -Path $nuget)) {
+    if (-not (Test-Path $toolDir)) {
+      New-Item -Path $toolDir -Type Directory | Out-Null
     }
 
-    Write-Output "Downloading NuGet.exe..."
-    Invoke-WebRequest -Uri $NUGET_URL -OutFile $NUGET_EXE
+    Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" `
+      -OutFile $nuget
   }
 }
 
-Pop-Location
+$packDir = Join-Path -Path $baseDir -ChildPath ".packages"
+
+if (-not (Test-Path $packDir)) {
+  New-Item -Path $packDir -ItemType Directory | Out-Null
+}
+
+$modulesDir = Join-Path -Path $baseDir -ChildPath Modules
 
 #------------------------------------------------------------------------------
 
@@ -54,7 +38,7 @@ Get-ChildItem -Path $modulesDir -Directory | ForEach-Object {
 
   Push-Location $(Join-Path -Path $modulesDir -ChildPath $id)
 
-  if (Test-Path ".\$id.psd1") {
+  if (Test-Path "./$id.psd1") {
     $data = Import-PowerShellDataFile .\$id.psd1
     $version = $data.ModuleVersion
     $author = $data.Author
@@ -66,12 +50,9 @@ Get-ChildItem -Path $modulesDir -Directory | ForEach-Object {
     $description = "A Julian Easterling Custom Powershell Module"
   }
 
-  Write-Output "##teamcity[blockOpened name='$id (v$version)']"
+  Remove-Item -Path "./package.nuspec" -Force -ErrorAction SilentlyContinue
 
-  Remove-Item *.nupkg -Force -ErrorAction SilentlyContinue
-  Remove-Item "package.nuspec" -Force -ErrorAction SilentlyContinue
-
-  Set-Content -Path "package.nuspec" -Value @"
+  Set-Content -Path "./package.nuspec" -Value @"
 <?xml version="1.0"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd">
   <metadata>
@@ -84,66 +65,70 @@ Get-ChildItem -Path $modulesDir -Directory | ForEach-Object {
     <copyright>(c) 2022 By Julian Easterling. Some rights reserved.</copyright>
 "@
 
-if (Test-Path -Path "./README.md") {
-  Add-Content -Path "package.nuspec" -Value "<readme>README.md</readme>"
-}
-
-if ($depends) {
-  Add-Content -Path "package.nuspec" -Value "<dependencies><group>"
-
-  foreach ($depend in $depends) {
-    Add-Content -Path "package.nuspec" `
-      -Value "<dependency id=`"$($depend.ModuleName)`" version=`"$($depend.ModuleVersion)`" />"
+  if (Test-Path -Path "./README.md") {
+    Add-Content -Path "./package.nuspec" -Value "<readme>README.md</readme>"
   }
 
-  Add-Content -Path "package.nuspec" -Value "</group></dependencies>"
-}
+  if ($depends) {
+    Add-Content -Path "./package.nuspec" -Value "<dependencies><group>"
 
-Add-Content -Path "package.nuspec" -Value @"
+    foreach ($depend in $depends) {
+      Add-Content -Path "./package.nuspec" `
+        -Value "<dependency id=`"$($depend.ModuleName)`" version=`"$($depend.ModuleVersion)`" />"
+    }
+
+    Add-Content -Path "./package.nuspec" -Value "</group></dependencies>"
+  }
+
+  Add-Content -Path "./package.nuspec" -Value @"
   </metadata>
   <files>
 "@
 
-(Get-ChildItem -Filter "*.md").Name | ForEach-Object {
-  if ($_) {
-    Add-Content -Path "package.nuspec" -Value "    <file src=`"$_`" />"
+  (Get-ChildItem -Filter "*.md").Name | ForEach-Object {
+    if ($_) {
+      Add-Content -Path "./package.nuspec" -Value "    <file src=`"$_`" />"
+    }
   }
-}
 
-(Get-ChildItem -Filter "*.psd1").Name | ForEach-Object {
-  if ($_) {
-    Add-Content -Path "package.nuspec" -Value "    <file src=`"$_`" />"
+  (Get-ChildItem -Filter "*.psd1").Name | ForEach-Object {
+    if ($_) {
+      Add-Content -Path "./package.nuspec" -Value "    <file src=`"$_`" />"
+    }
   }
-}
 
-(Get-ChildItem -Filter "*.psm1").Name | ForEach-Object {
-  if ($_) {
-    Add-Content -Path "package.nuspec" -Value "    <file src=`"$_`" />"
+  (Get-ChildItem -Filter "*.psm1").Name | ForEach-Object {
+    if ($_) {
+      Add-Content -Path "./package.nuspec" -Value "    <file src=`"$_`" />"
+    }
   }
-}
 
-(Get-ChildItem -Filter "*.ps1").Name | ForEach-Object {
-  if ($_) {
-    Add-Content -Path "package.nuspec" -Value "    <file src=`"$_`" />"
+  (Get-ChildItem -Filter "*.ps1").Name | ForEach-Object {
+    if ($_) {
+      Add-Content -Path "./package.nuspec" -Value "    <file src=`"$_`" />"
+    }
   }
-}
 
-Add-Content -Path "package.nuspec" -Value @"
+  Add-Content -Path "./package.nuspec" -Value @"
   </files>
 </package>
 "@
 
-  & "$NUGET_EXE" pack package.nuspec -Verbosity detailed -NoPackageAnalysis -NonInteractive -NoDefaultExcludes
+  if (Test-Path "Package.nuspec") {
+    $al = @(
+      "pack"
+      "Package.nuspec"
+      "-OutputDirectory $packDir"
+      "-Verbosity detailed"
+      "-NoPackageAnalysis"
+      "-NonInteractive"
+      "-NoDefaultExcludes"
+    )
 
-  Write-Output "`nPublishing '$id' v$version to $env:NuGetUrl"
+    Start-Process -FilePath $nuget -ArgumentList $al -NoNewWindow -Wait
+  }
 
-  & "$NUGET_EXE" push *.nupkg $env:NuGetApi -Source $env:NuGetUrl -skipDuplicate
-
-  Remove-Item *.nupkg -Force -ErrorAction SilentlyContinue
   Remove-Item "package.nuspec" -Force -ErrorAction SilentlyContinue
 
   Pop-Location
-
-  Write-Output "##teamcity[blockClosed name='$id (v$version)']"
-  Write-Output "`n`n"
 }
